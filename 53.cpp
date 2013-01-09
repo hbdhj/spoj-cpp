517508e6d996e8a9b5b96363d6aafb76c2e1e408
hbdhj <hbdhj@sina.com>
2013-01-09 20:34:00 +0800

    53 updated WA

--- 53.cpp
+++ 53.cpp
@@ -1,8 +1,8 @@
-#include <stdio>
+#include <stdio.h>
 int main() 
 {
-int i,j=0;char in[10][20];
-for(i= 0; i<10; i++)scanf("%s",in[i]);
-for(i= 0; i<10; i++){int r=1;while(in[i][j]!='\0'){char c=in[i][j];if ((c=='T')||(c=='D')||(c=='F')||(c=='F'))r*=2;j++;}printf("%d\n",r);}
-return 0;
+    int i,j=0;char in[10][20];
+    for(i=0;i<10; i++)scanf("%s",in[i]);
+    for(i=0;i<10; i++){int r=1;while(in[i][j]!='\0'){char c=in[i][j];if ((c=='T')||(c=='D')||(c=='F')||(c=='F'))r*=2;j++;}printf("%d\n",r);}
+    return 0;
 }
