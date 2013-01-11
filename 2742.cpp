5e30ba8985beb794986a604059511774bcfdcece
Deng Haijun <hbdhj@sina.com>
2013-01-11 10:35:29 +0800

    2742 WA

--- 2742.cpp
+++ 2742.cpp
@@ -24,25 +24,46 @@ Turn    Cow1  Cow2  Cow3
 #define MOD 98765431
 
 #include <stdio.h>
+#include <vector>
+
+using namespace std;
 
 int main()
 {
     unsigned long long int ci, sum;
     int i,n,t;
     scanf("%d %d", &n, &t);
-    vector<unsigned long long int> cows;
+    vector<unsigned long long int> cows(n);
+    sum=0;
     for(i=0;i<n;i++)
     {
         scanf("%llu", &cows[i]);
+        sum+=cows[i];
     }
-    printf("Turn    Cow1  Cow2  Cow3\n");
-    for(i=0;i<t+1;i++)
+    //printf("Turn    Cow1  Cow2  Cow3\n");
+    unsigned long long int total=1;
+    int ti=t;
+    while(ti--)
     {
-        printf(" %d       ",i);
-        for(j=0;j<n;j++)
-        {
-            if()
-        }
+        total*=(n-1);
     }
+    unsigned long long int self;
+    unsigned long long int other;
+    if(t%2)
+    {
+        other=(total+1)/n;
+        self=other-1;
+    }
+    else
+    {
+        other=(total-1)/n;
+        self=other+1;
+    }
+    //printf("sum = %d, total = %llu, self = %llu, other = %llu\n",sum, total, self, other);
+    for(i=0;i<n;i++)
+    {
+        printf("%llu\n",((sum-cows[i])*other+self*(cows[i]))%MOD);
+    }
+    
     return 0;
 }
