02b853a393ca75b2601a1028473784b8d0022b54
hbdhj <hbdhj@sina.com>
2013-01-12 17:39:52 +0800

    7169 WA

--- 237.cpp
+++ 237.cpp
@@ -1,28 +1,26 @@
+#include <stdio.h>
 #define F(n,t) for(n=0;n<t;n++)
+#define S(n) scanf("%d" &n);
 main()
 {
 int tN,ts,i,j,k,n;
-scanf("%d",tN);
+S(tN)
 int* nr= new int[tN];
-//for(i=0;i<tN;i++)
 F(i,tN)
 {
-scanf("%d",ts);
+scanf("%d",&ts);
 int ns=new int[ts+1];
-//for(j=0;j<ts;j++)
 F(j,ts)
 F(k,ts)
-//for(k=0;k<ts;k++)
 {
 if(k>j)||(k==0)
 ns[k]=0;
 else
 {
-scanf("%d",n);
-//if(ns[k]>ns)
+scanf("%d",&n);
 }
 }
 }
-for(i=0;i<tN;i++)
+F(i,tN)
 printf("%d\n",nr[i]);
 }
---
+++ 7169.cpp
@@ -0,0 +1,74 @@
+//
+//  7169.cpp
+//  
+//
+//  Created by Haijun Deng on 13-1-12.
+//  Copyright (c) 2013年 __MyCompanyName__. All rights reserved.
+//
+/*
+ Input:
+ 3
+ 1/2
+ 3/4
+ 3/4
+ Output:
+ 4
+
+ Input:
+ 5
+ 1/2
+ 3/4
+ 1/2
+ 1/4
+ 1/4
+ 
+ Output:
+ 4
+*/
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int i, friend_number, molecular, denominator;
+    scanf("%d", &friend_number);
+    int half=0;
+    int quater=0;
+    int threequater=0;
+    for(i=0;i<friend_number;i++)
+    {
+        scanf("%d/%d", &molecular, &denominator);
+        if((molecular+denominator)==3)
+            half++;
+        else if((molecular+denominator)==5)
+            quater++;
+        else if((molecular+denominator)==7)
+            threequater++;
+    }
+    int pies=1;
+    if(threequater<=quater)
+    {
+        pies+=threequater;
+        pies+=ceil(float(half)/2+float(quater)/4);
+        /*quater-=threequater;
+        if(half<=quater/2)
+        {
+            pies+=half;
+            quater-=half*2;
+            pies+=ceil(float(quater)/4);
+        }
+        else
+        {
+            pies+=half;
+            half-=ceil(float(quater)/2);
+            pies+=ceil(float(half)/2);
+        }*/
+        
+    }
+    else
+    {
+        pies+=threequater;
+        pies+=ceil(float(half)/2);
+    }
+    printf("%d\n", pies);
+}
