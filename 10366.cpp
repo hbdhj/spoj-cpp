924b63e3b6054f0e4ff117883717f58ec19affb8
hbdhj <hbdhj@sina.com>
2013-01-12 16:18:55 +0800

    10366 still WA with all known cases passed

--- 10366.cpp
+++ 10366.cpp
@@ -17,6 +17,38 @@ Friday
 
 using namespace std;
 
+int day_month[] ={31,28,31,30,31,30,31,31,30,31,30,31};
+
+bool isLeapYear(int year)
+{
+    if((year%4==0)&&(year%100!=0))
+        return true;
+    else if(year%400==0)
+        return true;
+    else
+        return false;
+}
+
+
+//get the days from 01/01/2000
+int getdays(int year, int month, int day)
+{
+    int passed_years=year-2000;
+    int passed_days=passed_years*365+passed_years/4-passed_years/100+passed_years/400;
+    //Add the one more for 2000 is a leap year
+    if(passed_years)
+        passed_days++;
+    //Add the one more for this leap year.
+    if(isLeapYear(year)&&(month<2))
+        passed_days--;
+    if(month>1)
+    while (--month) {
+        passed_days+=day_month[month-1];
+    }
+    passed_days+=day;
+    return passed_days;
+}
+
 int main ()
 {
     int year, month ,day, n;
@@ -27,7 +59,7 @@ int main ()
     vector<int> inputs(3*n);
     for(int i=0;i<n;i++)
     {
-        /* prompt user for date */
+        // prompt user for date
         scanf ("%d %d %d", &day, &month, &year);
         inputs[3*i]=year;
         inputs[3*i+1]=month;
@@ -35,19 +67,11 @@ int main ()
     }
     for(int i=0;i<n;i++)
     {    
-        time_t rawtime;
-        struct tm * timeinfo;
-        /* get current timeinfo and modify it to the user's choice */
-        time ( &rawtime );
-        timeinfo = localtime ( &rawtime );
-        timeinfo->tm_year = inputs[3*i] - 1900;
-        timeinfo->tm_mon = inputs[3*i+1] - 1;
-        timeinfo->tm_mday = inputs[3*i+2];
-
-        /* call mktime: timeinfo->tm_wday will be set */
-        mktime ( timeinfo );
-
-        printf ("%s.\n", weekday[timeinfo->tm_wday]);
+        year=inputs[3*i];
+        month=inputs[3*i+1];
+        day=inputs[3*i+2];
+        int passwd_days=getdays(year,month,day);
+        printf ("%s\n", weekday[(passwd_days+5)%7]);
     }
     return 0;
 }
