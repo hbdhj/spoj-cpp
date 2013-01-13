11a836ed132d98d8efba2a6af8efea6a96d1211b
hbdhj <hbdhj@sina.com>
2013-01-13 17:28:40 +0800

    5676 AC
    Silly problem

---
+++ 5676.cpp
@@ -0,0 +1,28 @@
+//
+//  5676.cpp
+//  
+//
+//  Created by Haijun Deng on 13-1-13.
+//  Copyright (c) 2013年 __MyCompanyName__. All rights reserved.
+//
+/*
+ */
+
+#include <iostream>
+
+int main()
+{
+    int i,j,tN,n,m;
+    scanf("%d", &tN);
+    for(i=0;i<tN;i++)
+    {
+        scanf("%d", &n);
+        for(j=0;j<n;j++)
+            scanf("%d", &m);
+    }
+    for(i=0;i<tN;i++)
+    {
+        printf("ALICE\n");
+    }
+    return 0;
+}
