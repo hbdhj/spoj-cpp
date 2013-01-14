da701639f2a0f7c8408e274842bd146afaceefb5
Deng Haijun <hbdhj@sina.com>
2013-01-14 10:58:53 +0800

    7975 updated with 2 more cases but still WA

--- 7975.cpp
+++ 7975.cpp
@@ -7,73 +7,91 @@
 //
 /*
  Input:
- 4
- 13 7 5
- 7 13 6
- 14 3 12
- 15 6 16
- 0
+4
+13 7 5
+7 13 6
+14 3 12
+15 6 16
+2
+-1 -1 -1
+-1 -1 -1
+4
+-1 -2 -3
+-4 -5 -6
+-1 -2 -3
+-7 -6 -5
+0
  Output:
- 1. 22
+1. 22
+2. -3
+3. -32
+
  */
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
+long long int minintwos(long long int a, long long int b)
+{
+    return a>b?b:a;
+}
+
+long long int mininthrees(long long int a, long long int b, long long int c)
+{
+    long long int min1=a>b?b:a;
+    return c>min1?min1:c;
+}
+
+long long int mininfours(long long int a, long long int b, long long int c, long long int d)
+{
+    long long int min1=mininthrees(a,b,c);
+    return min1>d?d:min1;
+}
+
 int main()
 {
     int tN,i,j;
     scanf("%d", &tN);
-    vector<vector<unsigned long long int> > inputs;
+    //vector<vector<unsigned long long int> > inputs;
+    vector<vector<long long int> > inputs;
     while (tN) {
-        vector<unsigned long long int> input(tN*3);
+        //vector<unsigned long long int> input(tN*3);
+        vector<long long int> input(tN*3);
         for(i=0;i<3*tN;i++)
         {
-            scanf("%llu", &input[i]);
+            scanf("%lld", &input[i]);
         }
         inputs.push_back(input);
         scanf("%d", &tN);
     }
     for(i=0;i<inputs.size();i++)
     {
-        for(j=2;j<inputs[i].size()/3-1;j++)
+        //first line
+        if(inputs[i][1]<0)
+            inputs[i][2]+=inputs[i][1];
+        //second line
+        inputs[i][3]+=inputs[i][1];
+        if(inputs[i][2]<0)
         {
-            //left one
-            if(inputs[i][3*j-3]<inputs[i][3*j-2])
-                inputs[i][3*j]+=inputs[i][3*j-3];
-            else
-                inputs[i][3*j]+=inputs[i][3*j-2];
-            //right one
-            if(inputs[i][3*j-1]<inputs[i][3*j-2])
-                inputs[i][3*j+2]+=inputs[i][3*j-1];
-            else
-                inputs[i][3*j+2]+=inputs[i][3*j-2];
-            //center one
-            if((inputs[i][3*j-3]<=inputs[i][3*j-2])&&(inputs[i][3*j-3]<=inputs[i][3*j-1]))
-                inputs[i][3*j+1]+=inputs[i][3*j-3];
-            else if((inputs[i][3*j-2]<=inputs[i][3*j-3])&&(inputs[i][3*j-2]<=inputs[i][3*j-1]))
-                inputs[i][3*j+1]+=inputs[i][3*j-2];
-            else if((inputs[i][3*j-1]<=inputs[i][3*j-3])&&(inputs[i][3*j-1]<=inputs[i][3*j-2]))
-                inputs[i][3*j+1]+=inputs[i][3*j-1];
-            //printf("%llu %llu %llu\n", inputs[i][3*j],inputs[i][3*j+1],inputs[i][3*j+2]);
-        }
-        // last line
-        unsigned long long int min;
-        int size=inputs[i].size();
-        
-        if(inputs[i].size()/3>2)
-        {
-            min=1000000000000;
-            for(j=4;j<7;j++)
-            {
-                if(min>inputs[i][size-j])
-                    min=inputs[i][size-j];
-            }
+            inputs[i][4]+=mininthrees(inputs[i][1], inputs[i][2], inputs[i][3]);
+            inputs[i][5]+=mininthrees(inputs[i][1], inputs[i][2], inputs[i][4]);
         }
         else
-            min=0;
-        printf("%d. %llu\n", i+1, min+inputs[i][1]+inputs[i][size-2]);
+        {
+            inputs[i][4]+=minintwos(inputs[i][1], inputs[i][3]);
+            inputs[i][5]+=minintwos(inputs[i][1], inputs[i][4]);
+        }
+        // laters
+        for(j=2;j<inputs[i].size()/3;j++)
+        {
+            inputs[i][3*j]+=inputs[i][3*j-2]>inputs[i][3*j-3]?inputs[i][3*j-3]:inputs[i][3*j-2];
+            inputs[i][3*j+1]+=mininfours(inputs[i][3*j-3], inputs[i][3*j-2], inputs[i][3*j-1],inputs[i][3*j]);
+            inputs[i][3*j+2]+=mininthrees(inputs[i][3*j+1], inputs[i][3*j-2], inputs[i][3*j-1]);
+        }
+        // print output
+        int size=inputs[i].size();
+        printf("%d. %lld\n", i+1, inputs[i][size-2]);
     }
     return 0;
 }
