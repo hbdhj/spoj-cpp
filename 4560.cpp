88e26909d27f3721780d4e4bc7e08fb79651c627
Deng Haijun <hbdhj@sina.com>
2013-01-25 15:41:37 +0800

    4560 AC

--- 4560.cpp
+++ 4560.cpp
@@ -25,7 +25,7 @@ using namespace std;
 
 int main()
 {
-    int n1, n2, n, i, j, k, sum1, sum2;
+    int n1, n2, n, i, j, k, sum1, sum2, ret;
     scanf("%d", &n1);
     vector<vector<int> > inputs;
     while (n1>0) {
@@ -43,36 +43,47 @@ int main()
         scanf("%d", &n1);
     }
     for (i=0; i<inputs.size()/2; i++) {
-        /*for (j=0; j<inputs[i*2].size(); j++) {
-            printf("%d ", inputs[i*2][j]);
-        }
-        printf("\n");
-        for (j=0; j<inputs[i*2+1].size(); j++) {
-            printf("%d ", inputs[i*2+1][j]);
-        }
-        printf("\n");*/
         j=0;
         k=0;
         sum1=0;
         sum2=0;
+        ret=0;
         while ((j<inputs[i*2].size())&&(k<inputs[i*2+1].size())) {
+            //printf("list1[%d] = %d, list2[%d] = %d, sum1 = %d, sum2 = %d\n", j, inputs[i*2][j], k, inputs[i*2+1][k], sum1, sum2);
             if(inputs[i*2][j]>inputs[i*2+1][k])
             {
+                sum2+=inputs[i*2+1][k];
                 k++;
-                sum2+=inputs[i*2][j];
             }
             else if(inputs[i*2][j]<inputs[i*2+1][k])
             {
-                j++;
                 sum1+=inputs[i*2][j];
+                j++;
             }
             else
             {
-                printf("list1[%d] = list2[%d] = %d, sum1 = %d, sum2 = %d\n", j, k, inputs[i*2][j], sum1, sum2);
+                ret+=inputs[i*2][j];
+                ret+=sum1>sum2?sum1:sum2;
+                //printf("---list1[%d] = list2[%d] = %d, sum1 = %d, sum2 = %d, ret=%d\n", j, k, inputs[i*2][j], sum1, sum2, ret);
                 k++;
                 j++;
+                sum1=0;
+                sum2=0;
             }
+            //printf("list1[%d] = %d, list2[%d] = %d, sum1 = %d, sum2 = %d\n", j, inputs[i*2][j], k, inputs[i*2+1][k], sum1, sum2);
         }
+        while(j<inputs[i*2].size())
+        {
+            sum1+=inputs[i*2][j];
+            j++;
+        }
+        while(k<inputs[i*2+1].size())
+        {
+            sum2+=inputs[i*2+1][k];
+            k++;
+        }
+        ret+=sum1>sum2?sum1:sum2;
+        printf("%d\n", ret);
     }
     return 0;
 }
