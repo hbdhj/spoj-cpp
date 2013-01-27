5dbd219e90c67a13ac2df3d1d1199c8df7e00e8b
hbdhj <hbdhj@sina.com>
2013-01-27 21:53:24 +0800

    C218 ongoing

---
+++ C218.cpp
@@ -0,0 +1,37 @@
+//
+//  C218.cpp
+//  
+//
+//  Created by Haijun Deng on 13-1-27.
+//  Copyright (c) 2013年 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;
+void PrintPoint(int n)
+{
+    if (n==2) {
+        printf("%.1f %.1f %.1f\n", 0.0, 0.0, 1.0);
+        printf("%.1f %.1f %.1f\n", 0.0, 0.0, -1.0);
+    }
+    else if(n==3)
+    {
+    }
+    else if(n==4)
+    {
+    }
+}
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    vector<int> inputs(n);
+    for (int i=0; i<n; i++) {
+        scanf("%d", &inputs[i]);
+    }
+    for (int i=0; i<n; i++) {
+        PrintPoint(inputs[i]);
+    }
+    return 0;
+}
