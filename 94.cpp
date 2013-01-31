cceba3d8483814703e6cbb4af2e3bb3a3c8d2ddb
Deng Haijun <hbdhj@sina.com>
2013-01-31 08:04:38 +0800

    94 AC

--- 94.cpp
+++ 94.cpp
@@ -16,81 +16,36 @@ Sample output:
 2
 1231200
 */
-#include <iostream>
-#include <vector>
-using namespace std;
-#define LINE_LENGTH 10
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <string.h>
 
-int getMayaBit(string& num)
-{
-    if(num=="S")
-        return 0;
-    else
-    {
-        int ret = 0;
-        for(int i=0; i<num.size(); i++)
-        {
-            if(num[i]=='.')
-                ret+=1;
-            else if(num[i]=='-')
-                ret+=5;
-        }
-        return ret;
-    }    
-}
+char maya[20][15] = {
+	"S", ".", "..", "...", "....",
+	"-", ". -", ".. -", "... -", ".... -",
+	"- -", ". - -", ".. - -", "... - -", ".... - -",
+	"- - -", ". - - -", ".. - - -", "... - - -", ".... - - -"
+};
 
-int getMayaNum(vector<string>& nums)
-{
-    int ret = 0;
-    int base =  1;
-    for(int i=nums.size()-1;i>=0;i--)
-    {
-        //cout<<getMayaBit(nums[i])<<endl;
-        ret += base*getMayaBit(nums[i]);
-        //cout<<"base = "<<base<<", Bit = "<<getMayaBit(nums[i])<<", ret = "<<ret<<endl;
-        if(i==(nums.size()-2))
-            base = base*18;
-        else
-            base = base*20;
-    }
-    return ret;
-}
+int pwr[7] = {1, 20, 360, 7200, 144000, 2880000, 57600000};
 
 int main()
 {
-    char t_N[5];
-    //cin.getline(t_N, 5);
-    int tN;
-    //sscanf(t_N, "%d", &tN);
-    scanf("%d", &tN);
-    vector<vector<string> > mayaNumStrList;
-    while(tN!=0)
-    {
-        vector<string> mayaNumStr;
-        cout<<"tN = "<<tN<<endl;
-        for(int i=0; i<tN; i++)
-        {
-            char input1[LINE_LENGTH];
-            char input2[LINE_LENGTH];
-            scanf("%s %s", input1, input2);
-            //cin.getline(input, LINE_LENGTH);
-            string str = input1;
-            str+=input2;
-            cout<<"str = "<<str<<endl;
-            mayaNumStr.push_back(str);
-        }
-        mayaNumStrList.push_back(mayaNumStr);
-        //cin.getline(t_N, 5);
-        scanf("%d", &tN);
-        scanf("%d", &tN);
-        //cin.getline(t_N, 5);
-        //sscanf(t_N, "%d", &tN);
-        //cout<<"tN = "<<tN<<endl;
-    }
-    for(int i=0;i<mayaNumStrList.size();i++)
-        //for(int j=0;j<mayaNumStrList[i].size();j++)
-            cout<<getMayaNum(mayaNumStrList[i])<<endl;
-    //vector<vector<int> > mayaNumList;
-    
-    return 0;
+	int n, res, i, j;
+	char temp[100];
+	while(scanf("%d", &n)==1 && n)
+	{
+		getchar();
+		res = 0;
+		for(i=n-1; i>=0; i--)
+		{
+			gets(temp);
+			for(j=0; j<20; j++)
+				if(!strcmp(temp, maya[j]))
+					break;
+			if(j) res += pwr[i]*j;
+		}
+		printf("%d\n", res);
+	}
+	return 0;
 }
