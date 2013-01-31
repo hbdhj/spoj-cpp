18cbdc5f6dbbfb56311362ad671f3dbe53247c9b
Deng Haijun <hbdhj@sina.com>
2013-01-31 10:23:27 +0800

    7975 AC

--- 7975.cpp
+++ 7975.cpp
@@ -27,71 +27,33 @@
 3. -32
 
  */
-#include <iostream>
-#include <vector>
+#include <stdio.h>
 
-using namespace std;
+const int inf = 0x7f7f7f7f;
+int tri[100000][3];
 
-long long int minintwos(long long int a, long long int b)
-{
-    return a>b?b:a;
+inline int min(int a, int b) {
+	return a < b ? a : b;
 }
 
-long long int mininthrees(long long int a, long long int b, long long int c)
-{
-    long long int min1=a>b?b:a;
-    return c>min1?min1:c;
-}
-
-long long int mininfours(long long int a, long long int b, long long int c, long long int d)
-{
-    long long int min1=mininthrees(a,b,c);
-    return min1>d?d:min1;
-}
-
-int main()
-{
-    int tN,i,j;
-    scanf("%d", &tN);
-    //vector<vector<unsigned long long int> > inputs;
-    vector<vector<long long int> > inputs;
-    while (tN) {
-        //vector<unsigned long long int> input(tN*3);
-        vector<long long int> input(tN*3);
-        for(i=0;i<3*tN;i++)
-        {
-            scanf("%lld", &input[i]);
-        }
-        inputs.push_back(input);
-        scanf("%d", &tN);
-    }
-    for(i=0;i<inputs.size();i++)
-    {
-        //first line
-        if(inputs[i][1]<0)
-            inputs[i][2]+=inputs[i][1];
-        //second line
-        inputs[i][3]+=inputs[i][1];
-        if(inputs[i][2]<0)
-        {
-            inputs[i][4]+=mininthrees(inputs[i][1], inputs[i][2], inputs[i][3]);
-            inputs[i][5]+=mininthrees(inputs[i][1], inputs[i][2], inputs[i][4]);
-        }
-        else
-        {
-            inputs[i][4]+=minintwos(inputs[i][1], inputs[i][3]);
-            inputs[i][5]+=minintwos(inputs[i][1], inputs[i][4]);
-        }
-        // laters
-        for(j=2;j<inputs[i].size()/3;j++)
-        {
-            inputs[i][3*j]+=inputs[i][3*j-2]>inputs[i][3*j-3]?inputs[i][3*j-3]:inputs[i][3*j-2];
-            inputs[i][3*j+1]+=mininfours(inputs[i][3*j-3], inputs[i][3*j-2], inputs[i][3*j-1],inputs[i][3*j]);
-            inputs[i][3*j+2]+=mininthrees(inputs[i][3*j+1], inputs[i][3*j-2], inputs[i][3*j-1]);
-        }
-        // print output
-        int size=inputs[i].size();
-        printf("%d. %lld\n", i+1, inputs[i][size-2]);
-    }
-    return 0;
+int main() {
+	int n, i, j, a, b, c, d, cs = 1;
+	while(scanf("%d", &n)==1 && n) {
+		for(i = 0; i < n; i++)
+			for(j = 0; j < 3; j++)
+				scanf("%d", &tri[i][j]);
+		tri[0][0] = inf;
+		tri[0][2] += tri[0][1];
+		for(i = 1; i < n; i++) {
+			for(j = 0; j < 3; j++) {
+				a = (j==2)? inf : tri[i-1][j+1];
+				b = tri[i-1][j];
+				c = (j==0)? inf : tri[i-1][j-1];
+				d = (j==0)? inf : tri[i][j-1];
+				tri[i][j] += min(min(a, b), min(c, d));
+			}
+		}
+		printf("%d. %d\n", cs++, tri[n-1][1]);
+	}
+	return 0;
 }
