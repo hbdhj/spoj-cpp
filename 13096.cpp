c8715786350409f2756084a0ce9b44c708fe16b7
Deng Haijun <hbdhj@sina.com>
2013-01-31 12:41:22 +0800

    13096 AC
    ad-hoc

--- 13096.cpp
+++ 13096.cpp
@@ -1,4 +1,6 @@
 /*
+13096. Amr Samir
+ad-hoc
 Sample Input
 2
 5
@@ -10,49 +12,75 @@ Sample Output
 2
 */
 
-#include <vector>
+#include <cassert>
+#include <cctype>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
 #include <iostream>
-
+#include <sstream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <list>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <iterator>
+#include <utility>
 using namespace std;
 
-int main()
-{
-    int i,t,j,n,ai;
-    scanf("%d", &t);
-    vector<vector<int> > inputs;
-    for(i=0;i<t;i++)
-    {
-        scanf("%d", &n);
-        vector<int> input(101, 0);
-        for(j=0;j<n;j++)
+template< class T > T _abs(T n) { return (n < 0 ? -n : n); }
+template< class T > T _max(T a, T b) { return (!(a < b) ? a : b); }
+template< class T > T _min(T a, T b) { return (a < b ? a : b); }
+template< class T > T sq(T x) { return x * x; }
+
+#define ALL(p) p.begin(),p.end()
+#define MP(x, y) make_pair(x, y)
+#define SET(p) memset(p, -1, sizeof(p))
+#define CLR(p) memset(p, 0, sizeof(p))
+#define MEM(p, v) memset(p, v, sizeof(p))
+#define CPY(d, s) memcpy(d, s, sizeof(s))
+#define READ(f) freopen(f, "r", stdin)
+#define WRITE(f) freopen(f, "w", stdout)
+#define SZ(c) (int)c.size()
+#define PB(x) push_back(x)
+#define ff first
+#define ss second
+#define i64 long long
+#define ld long double
+#define pii pair< int, int >
+#define psi pair< string, int >
+
+const double EPS = 1e-9;
+const int INF = 0x7f7f7f7f;
+
+
+int main() {
+	int T,n,u;
+	int cn[110];
+	cin>>T;
+	while(T--) {
+		cin>>n;
+		CLR(cn);
+		while(n--) {
+			cin>>u;
+			cn[u]++;
+		}
+		int i,r=-1,v=-1;
+		for(i=0;i<=100;i++)
         {
-            scanf("%d", &ai);
-            input[ai]++;
-        }
-        inputs.push_back(input);
-    }
-    for(i=0;i<t;i++)
-    {
-        int max,maxf;
-        maxf=0;
-        for(j=1;j<101;j++)
-        {
-            if(inputs[i][j])
+            if(cn[i] && !(cn[i]%i) && (cn[i]>r || (cn[i]==r && i<v))) 
             {
-                if(inputs[i][j]%j==0)
-                {
-                    if(maxf<=(inputs[i][j]/j))
-                    {
-                        maxf=inputs[i][j]/j;
-                        max=j;
-                    }
-                }
+                r=cn[i];
+                v=i;
             }
         }
-        if(max)
-        printf("%d\n", max);
-        else
-        printf("-1\n");        
-    }
-    return 0;
+		cout<<v<<'\n';
+	}
+	return 0;
 }
