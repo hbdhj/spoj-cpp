60f7716fadb5ea43089d137e73e5012a9b65f95f
Deng Haijun <hbdhj@sina.com>
2013-01-31 10:19:25 +0800

    4784 AC
    4784 brute force, 370 BFS, 10070 NC

--- 10070.cpp
+++ 10070.cpp
@@ -1,123 +1,56 @@
 /*
  * 2012/11/29
  */
-#include <iostream>
-#include <math.h>
-#include <vector>
-#include <sstream>
-#include <complex>
+#include <cstdio>
+#include <cmath>
 
-using namespace std;
+typedef struct { double x, y; } Point;
 
-#define TASK_NUM 10000
-#define COMPLEX complex<float> 
+const int MAX = 50000;
+const double EPS = 1e-12;
 
-COMPLEX get(COMPLEX a, COMPLEX b)
-{
-    if(a.real()==b.real())
-    {
-        return COMPLEX(a.real(), abs(a)>abs(b)?abs(a.imag()):abs(b.imag()));
-    }
-    /*if(a.imag()==b.imag())
-    {
-        return COMPLEX((a.real()+b.real())/2, abs(COMPLEX((b.real()-a.real())/2, a.imag())));
-    }*/
-    else
-    {
-        float x = (b.real()*b.real()+b.imag()*b.imag()-a.real()*a.real()-a.imag()*a.imag())/(b.real()-a.real())/2;
-        float minX, maxX, minY, maxY;
-        minX = b.real()>a.real()?a.real():b.real();
-        maxX = b.real()>a.real()?b.real():a.real();
-        minY = b.real()>a.real()?a.imag():b.imag();
-        maxY = b.real()>a.real()?b.imag():a.imag();
-        if(x<minX)
-        {
-            return COMPLEX(minX, abs(minY));
-        }
-        else if(x>maxX)
-        {
-            return COMPLEX(maxX, abs(maxY));
-        }
-        else{
-            COMPLEX m = COMPLEX(x, 0);
-            return COMPLEX(x, abs(a-m));
-        }
-    }
+Point P[MAX];
+int n;
+
+inline double distsq(const Point &a, const Point &b) {
+	return (a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y);
 }
 
-int main() 
-{
-    char home_number[7];
-    cin.getline (home_number,7); // Read the next number
-    int homeNumber;
-    sscanf (home_number,"%d", &homeNumber);    
-    vector<COMPLEX > outputs;
-    while (homeNumber!=0)
-    {
-        //string hN = home_number;
-        //input.push_back(hN);
-        COMPLEX r[2];
-        COMPLEX ret;
-        for(int i= 0; i<homeNumber; i++) 
-        {            
-            char in[30];
-            cin.getline (in, 30); // Read the next home location
-            
-            float c_i_r, c_i_i;
-            sscanf (in,"%f %f", &c_i_r, &c_i_i);
-            COMPLEX c_i(c_i_r, c_i_i);
-            
-            if(i<2)
-                r[i]=c_i;
-            if (i==1)
-            {
-                ret = get(r[0], r[1]);
-            }
-            else if(i>1) 
-            {
-                if(abs(COMPLEX(c_i.real()-ret.real(),c_i.imag()))>ret.imag())
-                {
-                    COMPLEX r1 = get(r[0], c_i);
-                    COMPLEX r2 = get(r[1], c_i);
-                    if(r1.imag()>r2.imag())
-                    {
-                        r2 = c_i;
-                    }
-                    else
-                    {
-                        r1 = c_i;
-                    }
-                    if(r1.imag()>ret.imag())
-                    {
-                        ret = r1;
-                    }
-                    else if(r2.imag()>ret.imag())
-                    {
-                        ret = r2;
-                    }
-                }
-            }
-            
-        }
-        if (homeNumber==1)
-        {
-            ret = COMPLEX(r[0].real(), abs(r[0].imag()));
-            outputs.push_back(ret);
-        }
-        else if (homeNumber==2)
-        {
-            ret = get(r[0], r[1]);
-            outputs.push_back(ret);
-        }
-        else
-            outputs.push_back(ret);
-        cin.getline (home_number,7); // Read the space
-        cin.getline (home_number,7); // Read the next home number
-        sscanf (home_number,"%d", &homeNumber);
-    }
-    for(int i = 0; i<outputs.size(); i++)
-    {
-        printf("%.9f %.9f\n", outputs[i].real(), outputs[i].imag());
-    }
-    return 0;
+double cost(double x) {
+	Point p = {x, 0};
+	double dist, mx = 0.0;
+	for(int i = 0; i < n; i++) {
+		dist = distsq(p, P[i]);
+		if(dist > mx) mx = dist;
+	}
+	return mx;
+}
+
+double ternary(double lt, double rt) {
+	int cnt = 64;
+	double ltthrd, rtthrd;
+	while(cnt--) {
+		ltthrd = (2.0 * lt + rt) / 3.0;
+		rtthrd = (lt + 2.0 * rt) / 3.0;
+		if(cost(ltthrd) < cost(rtthrd)) rt = rtthrd;
+		else lt = ltthrd;
+	}
+	return (lt + rt) / 2.0;
+}
+
+int main() {
+	double xmax, xmin, xsol;
+	while(scanf("%d", &n) == 1 && n) {
+		for(int i = 0; i < n; i++) {
+			scanf("%lf %lf", &P[i].x, &P[i].y);
+			if(!i) xmax = xmin = P[i].x;
+			else {
+				if(xmax < P[i].x) xmax = P[i].x;
+				if(xmin > P[i].x) xmin = P[i].x;
+			}
+		}
+		xsol = ternary(xmin, xmax);
+		printf("%.9lf %.9lf\n", xsol + EPS, sqrt(cost(xsol)) + EPS);
+	}
+	return 0;
 }
--- 4784.cpp
+++ 4784.cpp
@@ -1,19 +1,120 @@
-#include <iostream>
-#include <vector>
+/*
+4784. Diophantine equation
+Input:
+5
+0
+1
+10
+100
+1000
 
+Output:
+1
+4
+19
+148
+1476
+*/
+#if defined(_MSC_VER)
+#define _CRT_SECURE_NO_WARNINGS
+#include <boost/typeof/typeof.hpp>
+#define typeof BOOST_TYPEOF
+#endif
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <set>
+#include <map>
+#include <cstdio>
+#include <cstdlib>
+#include <cctype>
+#include <cmath>
+#include <ctime>
+#include <cassert>
+#include <numeric>
+#include <valarray>
+#include <complex>
+#include <memory.h>
 using namespace std;
 
-int main()
-{
-	vector<int> output(101, 1);
-	for(int i = 2; i<5; i++)
-	{
-		for(int j = i; j<100; )
-		{
-			output[j]++;
-			j+=i;
+const int dr[]={0,-1,0,1,-1,1, 1,-1};
+const int dc[]={1,0,-1,0, 1,1,-1,-1};
+const double eps=1e-9;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+typedef vector<bool> vb;
+typedef vector<vb> vvb;
+typedef vector<double> vd;
+typedef vector<vd> vvd;
+typedef vector<string> vs;
+typedef pair<int, int> pii;
+typedef vector<pii> vpii;
+typedef pair<double, double> pdd;
+typedef vector<pdd> vpdd;
+typedef complex<double> pnt;
+typedef vector<pnt> vpnt;
+typedef vector<vector<pair<int, long long> > > Graph;
+
+#define fr(i,a,b) for(int i(a),_b(b);i<=_b;++i)
+#define frd(i,a,b) for(int i(a),_b(b);i>=_b;--i)
+#define rep(i,n) for(int i(0),_n(n);i<_n;++i)
+#define reps(i,a) fr(i,0,sz(a)-1)
+#define fore(it,c) for(typeof((c).begin()) it=(c).begin();it!=(c).end();++it)
+#define all(a) a.begin(),a.end()
+#define pb push_back
+#define mp make_pair
+#define maximum(a) (*max_element(all(a)))
+#define minimum(a) (*min_element(all(a)))
+#define clr(x,a) memset(x,a,sizeof(x))
+#define findx(a,x) (find(all(a),x)-a.begin())
+#define two(X) (1LL<<(X))
+#define contain(S,X) (((S)&two(X))!=0)
+
+template<class T> void print(T const &x) {ostringstream os; os<<x; cout<<os.str()<<endl;}
+template<class T> void print(vector<T> const &v) {ostringstream os; for(int i=0; i<v.size(); ++i){if(i)os<<' ';os<<v[i];} cout<<os.str()<<endl;}
+template<class T> void print(vector<vector<T> > const &v){ostringstream os; for(int i=0;i<v.size();++i) {for(int j=0;j<v[i].size();++j){if(j)os<<' ';os<<v[i][j];}os<<endl;}cout<<os.str()<<endl;}
+template<class T> void print(valarray<T> const &v) {ostringstream os; for(int i=0; i<v.size(); ++i){if(i)os<<' ';os<<v[i];} cout<<os.str()<<endl;}
+template<class T> int sz(const T&c){return (int)c.size();}
+template<class T> void srt(T&c){std::sort(c.begin(),c.end());}
+template<class T> void checkmin(T &a,T b){if(b<a) a=b;}
+template<class T> void checkmax(T &a,T b){if(b>a) a=b;}
+template<class T> T sqr(T x){return x*x;}
+template<class T, class U> T cast (U x) { ostringstream os; os<<x; T res; istringstream is(os.str()); is>>res; return res; }
+template<class T> vector<T> split(string s, string x=" ") {vector<T> res; for(int i=0;i<s.size();i++){string a; while(i<(int)s.size()&&x.find(s[i])==string::npos)a+=s[i++]; if(!a.empty())res.push_back(cast<T>(a));} return res;}
+template<class T> T inside(T r,T c,T R, T C){return r>=0 && r<R && c>=0 && c<C;}
+
+
+int main( int argc, char* argv[] ) {
+	ios::sync_with_stdio(false);
+	int testCases;
+	scanf("%d", &testCases);
+	for(int testCase=1;testCase<=testCases;testCase++){
+		int n;
+		scanf("%d", &n);
+
+		ll res=0;
+
+		int nd=sqrt(sqrt(double(n)));
+		fr(d,0,nd){
+			int n1=n-d*d*d*d;
+			int nc=pow(n1,1./3)+1;
+			fr(c,0,nc){
+				int n2=n1-c*c*c;
+				if(n2<0)continue;
+				res+=sqrt(double(n2))+1;
+			}
 		}
+
+		printf("%lld\n", res);
 	}
-	for(int i = 0; i<output.size(); i++)
-		cout<<"output["<<i<<"] = "<<output[i]<<endl;
+
+	return 0;
 }
