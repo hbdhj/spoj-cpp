38a6c79e03ddf1d8c4e38a9dbcbc7b034d36c524
Deng Haijun <hbdhj@sina.com>
2013-01-31 09:40:46 +0800

    7742 AC
    Just use xor

--- 7742.cpp
+++ 7742.cpp
@@ -1,5 +1,6 @@
 /*
 7742. Onotole needs your help
+Just use xor
 Input:
 3
 1
@@ -8,48 +9,19 @@ Input:
 Output:
 8
 */
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
+#include <stdio.h>
 using namespace std;
 
-int main()
-{
-    int tN,i;
-    scanf("%d", &tN);
-    vector<int> inputs(tN);
-    for(i=0;i<tN;i++)
-    {
-        scanf("%d", &inputs[i]);
+int main() {
+    int N;
+    int tmp;
+    int X = 0;
+
+    scanf("%d", &N);
+    while(N--) {
+        scanf("%d", &tmp);
+        X = X ^ tmp;    //xor
     }
-    sort(inputs.begin(), inputs.end());
-    int check=0;
-    int checkNum=0;
-    for(i=0;i<tN;i++)
-    {
-        //printf("i = %d, inputs[%d] = %d, check = %d, checkNum = %d\n", i, i, inputs[i], check, checkNum);
-        if(check!=inputs[i])
-        {
-            if(checkNum==1)
-                break;
-            else if(checkNum==0)
-            {
-                checkNum++;
-                check=inputs[i];
-            }
-            else
-            {
-                checkNum=1;
-                check=inputs[i];
-            }
-        }
-        else
-            checkNum++;        
-    }
-    if(i!=tN-1)
-        printf("%d\n", check);
-    else
-        printf("%d\n", inputs[tN-1]);
+    printf("%d\n", X);
     return 0;
 }
