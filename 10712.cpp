0db16ca335258e22d9d370014837ea3fe0c7ef8b
hbdhj <hbdhj@sina.com>
2013-02-01 22:53:07 +0800

    10712 AC

--- 10712.cpp
+++ 10712.cpp
@@ -5,33 +5,36 @@ Input:
 1 1 20
 Output:
 19.441787
+ A*x+B*sin(x)=C
 */
-
-#include <cstdio>
-#include <vector>
-
+#include<iostream>
+#include<cmath>
+#include<cstdio>
 using namespace std;
 
-void Calculation(int A, int B, int C)
-{
-    float min=float(C-B)/A;
-    float max=float(C-B)/A;
-    printf("%.6f, %.6f\n", min, max);
+double orexp(double a, double b, double c, double x) {
+    double res = double((a * x) + (b * sin(x)) - c);
+    return res;
 }
 
-int main()
-{
-    int t, n, i, a[50];
-    scanf("%d", &t);
-    vector<int> inputs(3*t);
-    for(i=0;i<t;i++)
-    {
-        scanf("%d %d %d", &inputs[i*3],&inputs[i*3+1],&inputs[i*3+2]);
+double dfexp(double a, double b, double x) {
+    double res = a + (b * cos(x));
+    return res;
+}
+
+int main() {
+    int cases;
+    double a, b, c, x1, x2;
+    cin >> cases;
+    while(cases--) {
+        cin >> a >> b >> c;
+        x1 = double(c / a);
+        x2 = double(x1 - ((orexp(a, b, c, x1)) / (dfexp(a, b, x1))));
+        x1 = x2;
+        for(int i = 0; i < 100; i++) {
+            x2 = double(x1 - ((orexp(a, b, c, x1)) / (dfexp(a, b, x1))));
+            x1 = x2;
+        }
+        printf("%0.06lf\n", x2);
     }
-    for(i=0;i<t;i++)
-    {
-        Calculation(inputs[i*3],inputs[i*3+1],inputs[i*3+2]);
-    }
-    
-    return 0;
 }
