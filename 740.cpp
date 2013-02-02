9f64b56e39d2459309aadaae7336e4bc4d969082
Deng Haijun <hbdhj@sina.com>
2013-02-02 18:33:02 +0800

    760 WA
    DP but failed

--- 740.cpp
+++ 740.cpp
@@ -10,38 +10,35 @@ Input:
 Output:
 43
 */
-#include <iostream>
+#include <stdio.h>
 #include <vector>
 using namespace std;
 int main()
 {
-    unsigned long long int tN,n,k,h,t,a,r;
-    cin>>tN;
-    vector<int> ts;
-    for(int i=0;i<tN;i++)
-    {
-        cin>>n;
-        ts.push_back(n);        
-    }
-    k=0;
-    a=1;
-    h=0;
-    t=tN-1;
-    r=0;
-    while(k<tN)
-    {
-        if(ts[h]>ts[t])
-        {
-            r+=ts[t]*a;
-            t--;
-        }
-        else
-        {
-            r+=ts[h]*a;
-            h++;
-        }
-        a++;
-        k++;
-    }
-    cout<<r<<endl;
+	int i,j,b,n,r=0;
+	scanf("%d", &n);
+	vector<int> inputs(n);
+	for(i=0;i<n;i++)
+		scanf("%d", &inputs[i]);
+	i=0;
+	b=0;
+	j=n-1;
+	while(i<j)
+	{
+		b++;
+		if(inputs[i]<inputs[j])
+		{
+			r+=inputs[i]*b;
+			i++;
+		}
+		else
+		{
+			r+=inputs[j]*b;
+			j--;
+		}
+	}
+	b++;
+	r+=inputs[i]*b;
+	printf("%d\n",r);
+	return 0;
 }
