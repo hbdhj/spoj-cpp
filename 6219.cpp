088a05ea4ee24c73b7912ebaad37d0af88e34866
Deng Haijun <hbdhj@sina.com>
2013-02-02 16:29:21 +0800

    Remove 6219.h

--- 6219.cpp
+++ 6219.cpp
@@ -26,7 +26,7 @@ int main()
 	int test;
 
 	int len1, len2;
-	int val, i, j;
+	int val, i, j, l, m;
 	scanf("%d", &test);
 	while(test--)
 	{
@@ -45,6 +45,13 @@ int main()
             	{
             		val = (str1[i-1] == str2[j-1]) ? 0 : 1;
             		edit_value[i][j] = minimum(edit_value[i-1][j-1]+val , minimum(edit_value[i-1][j]+1, edit_value[i][j-1]+1));
+					/*printf("i=%d, j=%d val=%d edit_value[%d][%d]=%d edit_value[%][%d]=%d edit_value[%][%d]=%d edit_value[%d][%d]=%d\n",i,j,val,i,j,edit_value[i][j],i-1,j-1,edit_value[i-1][j-1],i-1,j,edit_value[i-1][j],i,j-1,edit_value[i][j-1]);
+					for(l=0 ; l<=len1 ; l++)
+					{
+						for(m=0 ; m<=len2 ; m++)
+							printf("%d ",edit_value[l][m]);
+						printf("\n");	
+					}*/
             	}
             	printf("%d\n", edit_value[len1][len2]);
 			}
