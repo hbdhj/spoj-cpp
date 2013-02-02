b9766b124a2b94d5872581ff7f8798e1419654a6
Deng Haijun <hbdhj@sina.com>
2013-02-02 20:35:41 +0800

    740 AC
    DP

--- 740.cpp
+++ 740.cpp
@@ -1,5 +1,6 @@
 /*
 740. Treats for the Cows
+DP
 Input:
 5
 1
@@ -10,35 +11,41 @@ Input:
 Output:
 43
 */
-#include <stdio.h>
+#include <cstdio>
 #include <vector>
+#define max(a,b) a>b?a:b
 using namespace std;
+int age = 1;
+vector<int> trts;
+vector<vector<int> > DP;
+int treats(int l,int r, int age)
+{
+  if(l==r)
+  {
+    DP[l][r] = trts[l]*age;
+    return trts[l]*age;
+  }
+  if(DP[l][r]!=0)
+    return DP[l][r];
+  DP[l+1][r] = treats(l+1,r,age+1);
+  DP[l][r-1] = treats(l,r-1,age+1);
+  int val = max(DP[l+1][r]+trts[l]*age,DP[l][r-1]+trts[r]*age);
+  return val;
+}
 int main()
 {
-	int i,j,b,n,r=0;
-	scanf("%d", &n);
-	vector<int> inputs(n);
-	for(i=0;i<n;i++)
-		scanf("%d", &inputs[i]);
-	i=0;
-	b=0;
-	j=n-1;
-	while(i<j)
-	{
-		b++;
-		if(inputs[i]<inputs[j])
-		{
-			r+=inputs[i]*b;
-			i++;
-		}
-		else
-		{
-			r+=inputs[j]*b;
-			j--;
-		}
-	}
-	b++;
-	r+=inputs[i]*b;
-	printf("%d\n",r);
-	return 0;
+  
+  int N,a;
+  scanf("%d",&N);
+  DP.resize(N);
+  for(int i=0;i<N;i++)
+      DP[i].resize(N);
+  trts.resize(N);
+  for(int i=0;i<N;i++)
+  {
+    scanf("%d",&a);
+    trts[i] = a;
+  }
+  printf("%d\n",treats(0,N-1,1));
+  return 0;
 }
