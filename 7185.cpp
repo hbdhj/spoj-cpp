d495bfaa7c1a9634e4093f049609fcfb641a927d
hbdhj <hbdhj@sina.com>
2013-02-06 22:21:55 +0800

    7185 AC

--- 7185.cpp
+++ 7185.cpp
@@ -15,87 +15,20 @@
  3 2 1 0
  0 0 0 12
  */
-#include <iostream>
-#include <vector>
+#include <stdio.h>
 
-using namespace std;
-
-int maxid(float e,float f,float s, float m)
-{
-    if(e>=f&&e>=s&&e>=m)
-        return 0;
-    else if(f>=e&&f>=s&&f>=m)
-        return 1;
-    else if(s>=e&&s>=f&&s>=m)
-        return 2;
-    else if(m>=f&&m>=s&&m>=e)
-        return 3;
-    
-}
-
-int gcd(int a, int b)
-{
-    if (b==0) {
-        return a;
-    }
-    else
-    {
-        return gcd(b, a%b);
-    }
-}
-
-int gtd(int a, int b)
-{
-    int c=gcd(a,b);
-    return a*b/c;
-}
-
-int cakes(int e,int f,int s, int m, int es, int fs, int ss, int ms)
-{
-    //printf("%d %d %d %d %d %d %d %d",e,f,s,m,es,fs,ss,ms);
-    float ed=float(e/es);
-    float fd=float(f/fs);
-    float sd=float(s/ss);
-    float md=float(m/ms);
-    int mid=maxid(ed, fd, sd, md);
-    int times;
-    switch (mid) {
-        case 0:
-            times=gtd(e,es)/es;
-            break;
-        case 1:
-            times=gtd(f,fs)/fs;
-            break;
-        case 2:
-            times=gtd(s,ss)/ss;
-            break;
-        case 3:
-            times=gtd(m,ms)/ms;
-            break;
-    }
-    printf("%d %d %d %d\n",es*times-e,fs*times-f,ss*times-s,ms*times-m);
-    return 0;
-}
-
-int main()
-{
-    int E, F, S, M, Es, Fs, Ss, Ms;
-    vector<int> ES,FS,SS,MS,ELs,FLs,SLs,MLs;
-    scanf("%d %d %d %d %d %d %d %d",&E,&F,&S,&M,&Es,&Fs,&Ss,&Ms);
-    while (E!=-1) {
-        ES.push_back(E);
-        FS.push_back(F);
-        SS.push_back(S);
-        MS.push_back(M);
-        ELs.push_back(Es);
-        FLs.push_back(Fs);
-        SLs.push_back(Ss);
-        MLs.push_back(Ms);
-        scanf("%d %d %d %d %d %d %d %d",&E,&F,&S,&M,&Es,&Fs,&Ss,&Ms);
-    }
-    for(int i=0;i<ES.size();i++)
-    {
-        cakes(ES[i],FS[i],SS[i],MS[i],ELs[i],FLs[i],SLs[i],MLs[i]);
-    }
-    return 0;
+int main() {
+	int a, b, c, d, aa, bb, cc, dd, at, bt, ct, dt, mx;
+	while(scanf("%d%d%d%d%d%d%d%d",&a, &b, &c, &d, &aa, &bb, &cc, &dd)==8 && a>=0) {
+		at = (a + aa - 1) / aa;
+		bt = (b + bb - 1) / bb;
+		ct = (c + cc - 1) / cc;
+		dt = (d + dd - 1) / dd;
+		mx = at;
+		mx = mx>bt? mx:bt;
+		mx = mx>ct? mx:ct;
+		mx = mx>dt? mx:dt;
+		printf("%d %d %d %d\n", aa*mx-a, bb*mx-b, cc*mx-c, dd*mx-d);
+	}
+	return 0;
 }
