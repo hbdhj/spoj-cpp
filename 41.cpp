e34ea03fd9d6d7ef262d1a91cb0d0343129480e6
Deng Haijun <hbdhj@sina.com>
2013-02-06 11:08:51 +0800

    41 still WA
    Not sample Eular graph

--- 41.cpp
+++ 41.cpp
@@ -1,7 +1,7 @@
 /*
 41. Play on Words
 Sample input:
-3
+6
 2
 acm
 ibm
@@ -12,6 +12,20 @@ mouse
 2
 ok
 ok
+3 
+aaa 
+bbb 
+ccc 
+4 
+boko 
+oring 
+kebo 
+goblok 
+4 
+masti 
+ishq 
+bc 
+cb 
 Sample output:
 The door cannot be opened.
 Ordering is possible.
@@ -33,27 +47,30 @@ int main()
 		scanf("%d",&wordNum[i]);
 		vector<int> starts(26,0);
 		vector<int> ends(26,0);
+        vector<int> loops(26,0);
 		for(j=0;j<wordNum[i];j++)
 		{
 			char word[1001];
 			scanf("%s", word);
-			if(word[0]!=word[strlen(word)-1])
+            if(word[0]!=word[strlen(word)-1])   // Not a loop
 			{
 				//printf("word[%d] (%c) != word[%d] (%c)\n",0,word[0],strlen(word)-1,word[strlen(word)-1]);
 				starts[word[0]-'a']++;
 				ends[word[strlen(word)-1]-'a']++;
 			}
-			else
+			else    //self loop
 			{
 				//printf("word[%d] (%c) == word[%d] (%c)\n",0,word[0],strlen(word)-1,word[strlen(word)-1]);
-				starts[word[0]-'a']--;
-				ends[word[strlen(word)-1]-'a']--;
+				//starts[word[0]-'a']--;
+				//ends[word[strlen(word)-1]-'a']--;
+                loops[word[0]-'a']++;
 			}
 		}
 		for(j=0;j<26;j++)
 		{
 			//printf("starts[%d](%c)=%d,ends[%d](%c)=%d\n",j,'a'+j,starts[j],j,'a'+j,ends[j]);
-			if((starts[j]<0||ends[j]<0))
+            printf("starts[%d](%c)=%d,ends[%d](%c)=%d,loop[%d](%c)=%d\n",j,'a'+j,starts[j],j,'a'+j,ends[j],j,'a'+j,loops[j]);
+			if(starts[j]==0&&ends[j]==0&&loops[j]!=0)
 			{
 				rets[i]=3;
 				break;
@@ -65,7 +82,7 @@ int main()
 				if(starts[j]<ends[j])
 					rets[i]+=ends[j]-starts[j];
 			}
-		}
+        }
 	}
 	for(i=0;i<t;i++)
 	{
