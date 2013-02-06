300be0cba6b1190b0366d34450ec9803d2b2c214
Deng Haijun <hbdhj@sina.com>
2013-02-06 13:30:00 +0800

    41 AC

--- 41.cpp
+++ 41.cpp
@@ -32,64 +32,84 @@ Ordering is possible.
 The door cannot be opened.
 */
 
-#include <stdio.h>
-#include <vector>
-#include <string.h>
+#include <cstdio>
+#include <queue>
+#include <cstring>
 using namespace std;
-int main()
-{
-	int i,j,t;
-	scanf("%d",&t);
-	vector<int> wordNum(t,0);
-	vector<int> rets(t,0);
-	for(i=0;i<t;i++)
-	{
-		scanf("%d",&wordNum[i]);
-		vector<int> starts(26,0);
-		vector<int> ends(26,0);
-        vector<int> loops(26,0);
-		for(j=0;j<wordNum[i];j++)
-		{
-			char word[1001];
-			scanf("%s", word);
-            if(word[0]!=word[strlen(word)-1])   // Not a loop
-			{
-				//printf("word[%d] (%c) != word[%d] (%c)\n",0,word[0],strlen(word)-1,word[strlen(word)-1]);
-				starts[word[0]-'a']++;
-				ends[word[strlen(word)-1]-'a']++;
-			}
-			else    //self loop
-			{
-				//printf("word[%d] (%c) == word[%d] (%c)\n",0,word[0],strlen(word)-1,word[strlen(word)-1]);
-				//starts[word[0]-'a']--;
-				//ends[word[strlen(word)-1]-'a']--;
-                loops[word[0]-'a']++;
+
+int indeg[26], outdeg[26], color[26];
+bool found[26], g[26][26];
+char word[1001];
+
+bool isConnected() {
+	int i, j;
+	queue< int > Q;
+	for(i=0; i<26; i++) 
+        color[i] = (found[i]? 0 : 2);
+	for(i=0; color[i]; i++); 
+        color[i] = 1;
+	Q.push(i);
+	while(!Q.empty()) 
+    {
+		i = Q.front(); 
+        Q.pop();
+		for(j=0; j<26; j++) 
+        {
+			if((g[i][j] || g[j][i]) && !color[j]) 
+            {
+				Q.push(j);
+				color[j] = 1;
 			}
 		}
-		for(j=0;j<26;j++)
-		{
-			//printf("starts[%d](%c)=%d,ends[%d](%c)=%d\n",j,'a'+j,starts[j],j,'a'+j,ends[j]);
-            printf("starts[%d](%c)=%d,ends[%d](%c)=%d,loop[%d](%c)=%d\n",j,'a'+j,starts[j],j,'a'+j,ends[j],j,'a'+j,loops[j]);
-			if(starts[j]==0&&ends[j]==0&&loops[j]!=0)
-			{
-				rets[i]=3;
-				break;
-			}
-			else if(starts[j]!=ends[j])
-			{
-				if(starts[j]>ends[j])
-					rets[i]+=starts[j]-ends[j];
-				if(starts[j]<ends[j])
-					rets[i]+=ends[j]-starts[j];
-			}
-        }
+		color[i] = 2;
 	}
-	for(i=0;i<t;i++)
-	{
-		if(rets[i]>2)
-			printf("The door cannot be opened.\n");
-		else
-			printf("Ordering is possible.\n");	
+	for(i=0; i<26; i++)
+		if(color[i]!=2)
+			return false;
+	return true;
+}
+
+bool hasEularPath() {
+	if(!isConnected()) 
+        return false;
+	int i, cnt1, cnt2;
+	for(i=cnt1=cnt2=0; i<26; i++) 
+    {
+		if(outdeg[i]-indeg[i]==1) 
+            cnt1++;
+		else 
+            if(indeg[i]-outdeg[i]==1) 
+                cnt2++;
+		else 
+            if(indeg[i]!=outdeg[i]) 
+                return false;
+	}
+	if(cnt1+cnt2==0 || (cnt1==1 && cnt2==1)) 
+        return true;
+	return false;
+}
+
+int main() {
+	int t, n, u, v, len;
+	scanf("%d", &t);
+	while(t--) {
+		memset(indeg, 0, sizeof indeg);
+		memset(outdeg, 0, sizeof outdeg);
+		memset(g, 0, sizeof g);
+		memset(found, 0, sizeof found);
+		scanf("%d", &n);
+		while(n--) 
+        {
+			scanf("%s", word);
+			len = strlen(word);
+			outdeg[u = word[0]-'a']++;
+			indeg[v = word[len-1]-'a']++;
+			g[u][v] = found[u] = found[v] = 1;
+		}
+		if(hasEularPath()) 
+            printf("Ordering is possible.\n");
+		else 
+            printf("The door cannot be opened.\n");
 	}
 	return 0;
 }
