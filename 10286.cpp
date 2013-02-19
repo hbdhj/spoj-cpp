0741c80bbb734e39542efb60c37f525240cb4593
Deng Haijun <hbdhj@sina.com>
2013-02-19 10:28:46 +0800

    10286 AC
    ad hoc

--- 10286.cpp
+++ 10286.cpp
@@ -2,27 +2,31 @@
 10286. DOTA HEROES
 */
 #include <iostream>
-#include <vector>
+#include <algorithm>
 using namespace std;
+
 int main()
 {
-    int t;
-    scanf("%d",&t);
-    vector<int> ns(t);
-    vector<int> ms(t);
-    vector<int> ds(t);
-    vector<vector<int> > hss(t);
-    for(i=0;i<t;i++)
+	/* Testcases, heroes, towers, damage */
+	int T, h, t, d, i, *max, list[500];
+	cin >> T;
+	while(T>0)
     {
-        scanf("%d %d %d",&ns[i],&ms[i],&ds[i]);
-        vector<int> hs(ns[i]);
-        for(j=0;j<ns[i];j++)
+		cin >> h >> t >> d;
+		for(i=0;i<h;i++)
+			cin >> list[i];
+
+		while(t>0)
         {
-            scanf("%d",&hs[j]);
-        }
-        hss.push_back(hs);
-    }
-    for(i=0;i<t;i++)
-    {
-    }
+			max = max_element(list, list+h);
+			if(d>=*max)
+				break;
+			*max = *max-d;
+			t--;
+		}
+
+		(t==0) ? cout << "YES" : cout << "NO";
+		cout << endl;
+		T--;
+	}
 }
