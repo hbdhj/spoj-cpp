c5f0d7c3b1cb042970f72412d4a9a92d95899de2
Deng Haijun <hbdhj@sina.com>
2013-02-27 22:36:38 +0800

    6288 still ongoing

--- 6288.cpp
+++ 6288.cpp
@@ -20,10 +20,28 @@ Output:
 #include <queue>
 #include <stdio.h>
 #include <iostream>
+
 using namespace std;
+
+/*class Node
+{
+public:
+	int id;
+	int number;
+};*/
+
+//class Leaf:public Node
+class Node
+{
+public:
+	int id;
+	int number;
+	vector<Node> leaves;
+};
+
 int main()
 {
-    int t,n,i;
+    int t,n,i,j,l;
     scanf("%d", &t);
     
     for(i=0;i<t;i++)
@@ -31,16 +49,89 @@ int main()
         scanf("%d",&n);
         if(n>1)
         {
-            queue<pair<int, int>> edges;
-            for(j=0j<n-1;j++)
+            vector<pair<int, int> > edges;
+            vector<int> conns(n,0);
+			vector<int> done(n,0);
+            for(j=0;j<n-1;j++)
             {
                 int f,s;
                 scanf("%d %d",&f, &s);
                 edges.push_back(pair<int, int>(f,s));
+				conns[f]++;
+				conns[s]++;
+			}
+            bool all=true;
+			for(j=0;j<n;j++)
+            {
+				printf("conns[%d] = %d\n", j, conns[j]);
+				if(conns[j]==1)
+				{
+					done[j]=1;
+				}
+				else
+				{
+					all=false;
+				}
             }
-            vector<int> conns(n);
-            for()
-        }
-        
+			int count=0;
+			while(all==false)
+			{
+				all=true;
+				for(j=0;j<n;j++)
+				{
+					printf("done[%d] = %d\n", j, done[j]);
+					if(done[j]==0)
+					{
+						int total=0;
+						bool got=true;
+						for(l=0;l<n-1;l++)
+						{
+							count++;
+							if(count>100)
+								break;
+							//printf("edges[%d].first=%d, edges[%d].second=%d\n",l,edges[l].first,l,edges[l].second);	
+							if(edges[l].first==j)
+							{
+								if(done[edges[l].second])
+									total+=done[edges[l].second]+1;
+								else
+								{
+									got=false;
+									break;
+								}
+							}
+							else if(edges[l].second==j)
+							{
+								if(done[edges[l].first])
+									total+=done[edges[l].first]+1;
+								else
+								{
+									got=false;
+									break;
+								}
+							}
+							
+						}
+						if(got)
+						{
+							done[j]=total+1;
+							printf("set done[%d]=%d\n",j,total+1);
+						}	
+						else
+						{
+							all=false;
+							break;
+						}
+					}
+				}
+			}
+			int max=0;
+			for(j=0;j<n;j++)
+			{
+				max+=done[j];
+			}
+			printf("%d\n",max);
+		}
     }
+	return 0;
 }
