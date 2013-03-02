6bfbe6265b6d6cc65acb812aeb2da7fb86644cd0
hbdhj <hbdhj@sina.com>
2013-03-02 20:59:50 +0800

    11354 AC
    easy

---
+++ 11354.cpp
@@ -0,0 +1,37 @@
+//
+//  11354.cpp
+//  
+//
+//  Created by Haijun Deng on 13-3-2.
+//  Copyright (c) 2013年 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    while (n--) 
+    {
+        unsigned long long k;
+        scanf("%llu",&k);
+        k++;
+        vector<int> output;
+        while (k>1) 
+        {
+            if(k%2)
+                output.push_back(6);
+            else
+                output.push_back(5);
+            k=k/2;
+        }
+        for (int i=output.size(); i>0; i--)
+        {
+            printf("%d", output[i-1]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
