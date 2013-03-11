f33afb81e25eb7d6621d63cb0acf6017349ab51e
hbdhj <hbdhj@sina.com>
2013-03-11 22:32:45 +0800

    3881 ongoig

---
+++ 3881.cpp
@@ -0,0 +1,45 @@
+//
+//  3881.cpp
+//  
+//
+//  Created by Haijun Deng on 13-3-11.
+//  Copyright (c) 2013年 __MyCompanyName__. All rights reserved.
+//
+/*
+ Input
+ 5
+ SSPPR
+ 1
+ SSPPR
+ 
+ Output
+ 5
+ 10
+ 
+ 
+ Input
+ 5
+ SSPPR
+ 2
+ PPRRS
+ RRSSP
+ 
+ Output
+ 10
+ 15
+ 
+ 
+ Input
+ 4
+ SPRS
+ 4
+ RPRP
+ SRRR
+ SSPR
+ PSPS
+ 
+ Output
+ 12
+ 21
+ */
+#include <iostream>
