6d1bf99370c21850fd5b3ea575fc189451b8e124
hbdhj <hbdhj@sina.com>
2013-03-16 22:47:23 +0800

    154 AC

---
+++ 154_ROCK.cpp
@@ -0,0 +1,40 @@
+//
+//  154_ROCK.cpp
+//  
+//
+//  Created by Haijun Deng on 13-3-16.
+//  Copyright (c) 2013年 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+template< class T > void setmax(T &a, T b) { if(a < b) a = b; }
+int main() 
+{
+	int t, n, i, j, add, cnt;
+	int memo[256];
+	char rock[256];
+	scanf("%d", &t);
+	while(t--) 
+    {
+		scanf("%d%s", &n, rock);
+		memo[0] = rock[0]-'0';
+		for(i = 1; i < n; i++) 
+        {
+			memo[i] = memo[i-1];
+			cnt = 0;
+			for(j = i; j >= 0; j--) 
+            {
+				cnt += ((rock[j]=='1')? 1 : -1);
+				if(cnt > 0) 
+                {
+					add = i - j + 1;
+					if(j > 0) 
+                        add += memo[j-1];
+					setmax(memo[i], add);
+				}
+			}
+		}
+		printf("%d\n", memo[n-1]);
+	}
+	return 0;
+}
