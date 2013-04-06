efe0622393706c664b5e5a0d4d650b8a79903372
hbdhj <hbdhj@sina.com>
2013-04-06 17:35:09 +0800

    63 9032 AC

---
+++ 63_SQRBR.cpp
@@ -0,0 +1,70 @@
+//
+//  63_SQRBR.cpp
+//  
+//
+//  Created by Haijun Deng on 13-4-5.
+//  Copyright (c) 2013年 __MyCompanyName__. All rights reserved.
+//
+/*
+ ALGO: dynamic programming
+ Sample input:
+ 5 
+ 1 1 
+ 1 
+ 1 1 
+ 2 
+ 2 1 
+ 1 
+ 3 1 
+ 2 
+ 4 2 
+ 5 7 
+ 
+ Sample output:
+ 1 
+ 0 
+ 2 
+ 3 
+ 2 
+
+ */
+#include <iostream>
+
+const int MAX = 40;
+
+bool f[MAX];
+int dp[MAX][MAX], n;
+
+long long solve(int pos, int open) 
+{
+	if(open < 0) 
+        return 0; // invalid
+	if(pos == n) 
+        return open == 0; // must for being valid
+	int &ret = dp[pos][open];
+	if(ret != -1) 
+        return ret;
+	if(f[pos]) 
+        return ret = solve(pos+1, open+1); // must put a [ here
+	return ret = solve(pos+1, open+1) + solve(pos+1, open-1); // try [ and ] both
+}
+
+int main() 
+{
+	int t, k, m, i;
+	scanf("%d", &t);
+	while(t--) 
+    {
+		scanf("%d%d", &n, &k);
+		n <<= 1;
+        memset(f, 0, sizeof(f));
+		memset(dp, -1, sizeof(dp));
+        for(i = 0; i < k; i++) 
+        {
+			scanf("%d", &m);
+			f[m-1] = 1;
+		}
+		printf("%lld\n", solve(0, 0));
+	}
+	return 0;
+}
---
+++ 9032_CUBEFR.cpp
@@ -0,0 +1,66 @@
+//
+//  9032_CUBEFR.cpp
+//  
+//
+//  Created by Haijun Deng on 13-4-6.
+//  Copyright (c) 2013年 __MyCompanyName__. All rights reserved.
+//
+/*
+ algo: sieve
+ Sample Input: 
+ 10
+ 1
+ 2
+ 3
+ 4
+ 5
+ 6
+ 7
+ 8
+ 9
+ 10
+ 
+ Sample Output:
+ 
+ Case 1: 1
+ Case 2: 2
+ Case 3: 3
+ Case 4: 4
+ Case 5: 5
+ Case 6: 6
+ Case 7: 7
+ Case 8: Not Cube Free
+ Case 9: 8
+ Case 10: 9
+ */
+#include <cstdio>
+#include <cstdlib>
+
+using namespace std;
+
+const int N = 1100000;
+int cube[N];
+int id[N], num = 0;
+
+int main() 
+{
+    // precalc.
+    for (int i = 2; i*i*i < N; ++i)
+        for (int j = i*i*i; j < N; j+=i*i*i)
+            cube[j] = 1;
+    for (int i = 1; i < N; ++i)
+        if (!cube[i]) 
+            id[i] = ++num;
+    
+    int T; 
+    scanf("%d", &T);
+    for (int t = 1; t <= T; ++t) 
+    {
+        int n; scanf("%d", &n);
+        printf("Case %d: ", t);
+        if (!cube[n]) 
+            printf("%d\n", id[n]);
+        else 
+            printf("Not Cube Free\n");
+    }
+}
