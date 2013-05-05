bc5896cb810b98b8d58b53f96e5279839b7a1113
hbdhj <hbdhj@sina.com>
2013-05-05 19:27:14 +0800

    4177 AC

---
+++ 4177_HERDING.cpp
@@ -0,0 +1,66 @@
+//
+//  4177_HERDING.cpp
+//  
+//
+//  Created by Haijun Deng on 13-5-5.
+//  Copyright (c) 2013年 __MyCompanyName__. All rights reserved.
+//
+
+/*
+ ALGO: flood fill
+ Input:
+ 3 4
+ SWWW
+ SEWN
+ EEEN
+ 
+ Output:
+ 2
+ */
+
+#include <cstdio>
+using namespace std;
+
+#define MAX 1000
+
+char grid[MAX][MAX+1];
+int R, C, trap[MAX][MAX], count, last;
+
+void floodfill(int i, int j)
+{
+	if(i>=0 && j>=0 && i<R && j<C)
+	{
+		if(trap[i][j])
+		{
+			count = trap[i][j];
+			return;
+		}
+		trap[i][j] = count;
+		if(grid[i][j]=='W') 
+            floodfill(i, j-1);
+		else if(grid[i][j]=='E') 
+            floodfill(i, j+1);
+		else if(grid[i][j]=='N') 
+            floodfill(i-1, j);
+		else if(grid[i][j]=='S') 
+            floodfill(i+1, j);
+		trap[i][j] = count;
+	}
+}
+
+int main()
+{
+	scanf("%d%d", &R, &C);
+	for(int i=0; i<R; i++) scanf("%s", grid[i]);
+	for(int i=0; i<R; i++)
+        for(int j=0; j<C; j++)
+            if(!trap[i][j])
+            {
+                count = last + 1;
+                floodfill(i, j);
+                if(count > last) 
+                    last++;
+            }
+	printf("%d\n", last);
+	return 0;
+}
