3fc398ac5c2bae0f2201d208fa2aeda3c1fd13b4
hbdhj <hbdhj@sina.com>
2013-05-20 21:08:16 +0800

    14932 ongoing

---
+++ 14932_LCA.cpp
@@ -0,0 +1,58 @@
+//
+//  14932_LCA.cpp
+//  
+//
+//  Created by Haijun Deng on 13-5-20.
+//  Copyright (c) 2013年 __MyCompanyName__. All rights reserved.
+//
+/*
+ TASK: Lowest Common Ancestor
+ Input:
+ 1
+ 7
+ 3 2 3 4
+ 0
+ 3 5 6 7
+ 0
+ 0
+ 0
+ 0
+ 2
+ 5 7
+ 2 7
+ 
+ Output:
+ Case 1:
+ 3
+ 1
+ */
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        int n;
+        cin>>n;
+        vector<vector<int>> tree(n, vector<int>(0,0));
+        for(int i=0;i<n;i++)
+        {
+            
+        }
+        int m;
+        cin>>m;
+        vector<pair<int, int>> queries;
+        for(int i=0;i<m;i++)
+        {
+            int s_n,t_n;
+            cin>>s_n>>t_n;
+            
+        }
+        
+    }
+}
