b5624c21660e656000bd86a03d9856d89ea59a91
hbdhj <hbdhj@sina.com>
2013-06-04 22:31:29 +0800

    AC

---
+++ 3943_MDOLLS.cpp
@@ -0,0 +1,64 @@
+/*
+TASK: MDOLLS
+ALGO: sorting, greedy
+ SAMPLE INPUT
+ 4
+ 3
+ 20 30 40 50 30 40
+ 4
+ 20 30 10 10 30 20 40 50
+ 3
+ 10 30 20 20 30 10
+ 4
+ 10 10 20 30 40 50 39 51    
+ SAMPLE OUTPUT
+ 1
+ 2
+ 3
+ 2  
+*/
+
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+
+const int MAX = 20000;
+
+typedef struct { int ff, ss; } pii;
+
+inline bool scomp(const pii &a, const pii &b) 
+{
+    return a.ss != b.ss ? a.ss > b.ss : a.ff < b.ff;
+}
+
+inline bool bcomp(const pii &a, const pii &b)
+{
+    return a.ff < b.ff;
+}
+
+int main() 
+{
+    int test, i, j, k, n, ans;
+    pii p[MAX];
+    scanf("%d", &test);
+    while(test--) 
+    {
+        scanf("%d", &n);
+        for(i = 0; i < n; i++) 
+            scanf("%d %d", &p[i].ff, &p[i].ss);
+        sort(p, p + n, scomp);
+        for(i = j = 0, ans = n; i < n; i++) 
+        {
+            k = upper_bound(p, p+j, p[i], bcomp) - p;
+            if(k == j) 
+                p[j++] = p[i];
+            else
+            {
+                ans--;
+                p[k].ff = p[i].ff;
+            }
+        }
+        printf("%d\n", ans);
+    }
+    return 0;
+}
---
+++ 7881_C1LJUTNJ.cpp
@@ -0,0 +1,58 @@
+//
+//  7881_C1LJUTNJ.cpp
+//  
+//
+//  Created by Haijun Deng on 13-6-4.
+//  Copyright (c) 2013年 __MyCompanyName__. All rights reserved.
+//
+/*
+ TASK: Ljutnja
+ ALGO: sorting
+ Input:
+ 5 3 
+ 1 
+ 3 
+ 2
+ Output:
+ 1
+ 
+ Input:
+ 10 4
+ 4
+ 5
+ 2
+ 3
+ 
+ Output:
+ 4
+ */
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int a[100000];
+
+int main() 
+{
+	int n, m, i, ang;
+	long long tot = 0, ans = 0;
+	scanf("%d %d", &m, &n);
+	for(i = 0; i < n; i++) 
+    {
+		scanf("%d", &a[i]);
+		tot += a[i];
+	}
+	tot -= m;
+	sort(a, a + n);
+	for(i = 0; i < n; i++) 
+    {
+		ang = a[i]>(tot/(n-i))?(tot/(n-i)):a[i];
+		ans += ang * ang;
+		tot -= ang;
+        //printf("ang = %d, ans = %lld, tol= %lld \n", ang, ans, tot);
+	}
+	printf("%lld\n", ans);
+	return 0;
+}
