69b13321d739f956669291fb970834ff25a22a1a
hbdhj <hbdhj@sina.com>
2013-06-04 23:00:30 +0800

    AC

---
+++ 11769_RPLK.cpp
@@ -0,0 +1,66 @@
+/*
+TASK: Kind and gently
+ALGO: sorting
+ INPUT
+ 3
+ 5 1 1 3
+ 6 2
+ 5 10
+ 4 20
+ 3 15
+ 1 1
+ 3 1 0 5
+ 3 15
+ 2 20
+ 1 60
+ 2 1 1 25
+ 15 10
+ 12 10
+ 
+ OUTPUT
+ Scenario #1: 19
+ Scenario #2: 15
+ Scenario #3: 145
+*/
+
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+
+struct Wood { int h, c; };
+
+Wood wood[100000];
+
+inline bool comp(const Wood &a, const Wood &b) 
+{
+	return a.h != b.h ? a.h > b.h : a.c > b.c;
+}
+
+int main()
+{
+	int test, cs, e, w, i, k, total, cnt, tmp, hi, wi, h, m;
+	scanf("%d", &test);
+	for(cs = 1; cs <= test; cs++) 
+    {
+		scanf("%d %d %d %d", &e, &m, &h, &w);
+		for(i = k = 0; i < e; i++) 
+        {
+			scanf("%d %d", &hi, &wi);
+			if(wi / (1 + m) > 0)
+            {
+				wood[k].h = hi, wood[k].c = wi / (1 + m);
+				k++;
+			}
+		}
+		sort(wood, wood + k, comp);
+		for(total = i = cnt = 0; i < k && cnt < w; i++) 
+        {
+			tmp = min(wood[i].c, w - cnt);
+			cnt += tmp;
+			total += wood[i].h * tmp;
+		}
+		total += cnt * h;
+		printf("Scenario #%d: %d\n", cs, total);
+	}
+	return 0;
+}
---
+++ 13090_PCPC12D.cpp
@@ -0,0 +1,59 @@
+/*
+TASK: New Strategy
+ALGO: sorting
+*/
+#include <cstdio>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+const int MAX = 1024;
+const int LEN = 256;
+
+char str[MAX][LEN];
+int cnt[MAX][26];
+int idx[MAX];
+
+bool comp(int i, int j) 
+{
+	for(int k = 0; k < 26; k++) {
+		if(cnt[i][k] > cnt[j][k]) 
+            return true;
+		if(cnt[i][k] < cnt[j][k]) 
+            return false;
+	}
+	return i<j;
+}
+
+int main() 
+{
+	int test, n, i, j, cs;
+	char buff[2];
+	scanf("%d", &test);
+	for(cs = 1; cs <= test; cs++) 
+    {
+		scanf("%d", &n);
+		gets(str[0]);
+		for(i = 0; i < n; i++) 
+        {
+			gets(str[i]);
+			idx[i] = i;
+			memset(cnt[i],0,sizeof(cnt[i]));
+			for(j = 0; str[i][j]; j++) 
+            {
+				if(isalpha(str[i][j])) 
+                {
+					cnt[i][tolower(str[i][j])-'a']++;
+				}
+			}
+		}
+		sort(idx, idx + n, comp);
+		printf("case: %d\n", cs);
+		for(i = 0; i < n; i++) 
+        {
+			printf("%s\n", str[idx[i]]);
+		}
+	}
+	return 0;
+}
