aff2dcb9f2a88787d8385713be93846e2ae16bf8
hbdhj <hbdhj@sina.com>
2013-06-12 17:07:03 +0800

    7741 AC

--- 14972_UOFTAC.cpp
+++ 14972_UOFTAC.cpp
@@ -26,6 +26,8 @@ Output:
 
 */
 
+#include <stdio.h>
+
 #include <iostream>
 #include <vector>
 
@@ -49,74 +51,78 @@ int main()
 		for(int i=0; i<s; i++)
 		{
 			cin>>steps[i];
+			//printf("s_h = %d, s_w = %d before the step %d - %c, t_n = %d\n", s_h, s_w, i+1, steps[i], t_n);
 			switch(steps[i])
 			{
-				case D:
+				case 'D':
 					if((s_h+1)<h) //Not the bottom
 					{
 						switch(underground[s_h+1][s_w])
 						{
-							case T:
+							case 'T':
 								t_n++;
 								s_h++;
+								underground[s_h][s_w]='D';
 								break;
-							case S:
+							case 'S':
 								break;
-							case D:
+							case 'D':
+							case 'E':
 								s_h++;
-								break;	
-							case E:
-								while((s_h+1)<h)
-								{
-									if(underground[s_h+1][s_w]!='E')
-										break;
-									else
-										s_h++;
-								}
 								break;	
 						}
 					}
 					break;
-				case L:
-					if(s_h>=0) //Not the bottom
+				case 'L':
+					if(s_h>=0&&s_w) //Not the bottom
 					{
 						switch(underground[s_h][s_w-1])
 						{
-							case T:
+							case 'T':
 								t_n++;
 								s_w--;
+								underground[s_h][s_w]='D';
 								break;
-							case S:
+							case 'S':
 								break;
-							case D:
+							case 'D':
+							case 'E':
 								s_w--;
 								break;	
-							case E:
-								s_w--;
-								while((s_h+1)<h)
-								{
-									if(underground[s_h][s_w]!='E')
-										break;
-									else
-										s_h++;
-								}
-								break;	
-						}
+                        }
 					}
 					else if(s_w)
 						s_w--;
 					break;
-				case R:
-					if(s_h>=0) //Not the bottom
+				case 'R':
+					if(s_h>=0&&s_w<w-1) //Not the bottom
 					{
-						
+						switch(underground[s_h][s_w+1])
+						{
+							case 'T':
+								t_n++;
+								s_w++;
+								underground[s_h][s_w]='D';
+								break;
+							case 'S':
+								break;
+							case 'D':
+							case 'E':
+								s_w++;
+								break;	
+						}
 					}
 					else if(s_w<w-1)
 						s_w++;
 					break;
 			}
-			
+			//fall down
+			while((s_h<h-1)&&(underground[s_h+1][s_w]=='E'))
+				s_h++;
+			//printf("s_h = %d, s_w = %d after the step %d - %c, t_n = %d\n", s_h, s_w, i+1, steps[i], t_n);
 		}
+		printf("%d\n",t_n);
 	}
 	return 0;
 }
+
