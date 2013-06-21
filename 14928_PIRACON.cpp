62d45abdbbdae9e4dcaf4972493152a9e511c43a
hbdhj <hbdhj@sina.com>
2013-06-21 23:10:13 +0800

    14928 WA

---
+++ 14928_PIRACON.cpp
@@ -0,0 +1,43 @@
+/*
+ TASK: Pyramidal Constructions
+ Input:
+ 2
+ 1
+ 4
+ 
+ Output:
+ Pyramid E. Nro# 1: 1
+ Tringus: 0
+ Pyramid E. Nro# 2: 68
+ Tringus: 24
+
+*/
+#include <iostream>
+#include <vector>
+using namespace std;
+#define MAX 1000000
+unsigned long long f[MAX];
+unsigned long long e[MAX];
+
+void p()
+{
+f[0]=1;
+e[0]=0;
+for (int i=1; i<MAX; i++) 
+{
+f[i]=(i+1)*(i+1)+i*i+f[i-1];
+e[i]=((i+1)*(i+1)*(i+1)-f[i])*2;
+}
+};
+
+int main()
+{
+int t,i,j;
+scanf("%d",&t);
+p();
+for (i=0; i<t; i++) {
+scanf("%d",&j);
+printf("Pyramid E. Nro# %d: %llu\nTringus: %llu\n", i+1, f[j-1]+e[j-1], e[j-1]);
+}
+return 0;
+}
