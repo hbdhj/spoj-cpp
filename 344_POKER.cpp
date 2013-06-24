aa7df1a6c72658d5427b29eab39800a8b40ffd28
hbdhj <hbdhj@sina.com>
2013-06-24 22:29:32 +0800

    344 ongoing

---
+++ 344_POKER.cpp
@@ -0,0 +1,57 @@
+//
+//  344_POKER.cpp
+//  
+//
+//  Created by Haijun Deng on 13-6-24.
+//  Copyright (c) 2013年 __MyCompanyName__. All rights reserved.
+//
+/*
+ TASK: poker
+ LOGIC: 
+ 1. royal flush - ace, king, queen, jack and ten, all in the same suit
+ 2. straight flush - five cards of the same suit in sequence, such as 10,9,8,7,6 of clubs; ace can be counted both as the highest card or as the lowest card - A,2,3,4,5 of hearts is a straight flush. But 4,3,2,A,K of hearts is not a straight flush - it's just a flush.
+ 3. four of a kind - four cards of the same rank, such as four kings.
+ 4. full house - three cards of one rank plus two cards of another rank
+ 5. flush - five cards of the same suit (but not a straight flush)
+ 6. straight - five cards in order - just like the straight flush, but mixed suits
+ 7. three of a kind - three cards of one rank and two other cards
+ 8. two pairs - two cards of one rank, two cards of another rank, and one more card
+ 9. pair - two cards of the same rank
+ 10. high card - none of the above
+ 
+ Input:
+ 4
+ AH KH QH TH JH
+ AD 2D 3D 4D 5D
+ 2S 2S 2S 2S 4D
+ QH QD 2S QC 2C
+ 4C 9C TC AC KC
+ KH AS 2C 3C 4D
+ 5H 5D 5C 7H KS
+ KH 5S 3C 5C 7D
+ 2H 2S 4C 4S 5D
+ AH 4S 9C TD 3H
+ 
+ Output:
+ royal flush
+ straight flush
+ four of a kind
+ full house
+ flush
+ straight
+ three of a kind
+ two pairs
+ pair
+ high card
+ */
+#include <iostream>
+
+int process(string str)
+{
+    return 0;
+}
+
+int main()
+{
+    return 0;
+}
