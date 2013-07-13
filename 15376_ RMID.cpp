10c593505608637aeb2a28bbb1b525339cfdd764
hbdhj <hbdhj@sina.com>
2013-07-14 07:32:37 +0800

    15376 ongoing

---
+++ 15376_ RMID.cpp
@@ -0,0 +1,55 @@
+//
+//  15376_ RMID.cpp
+//  
+//
+//  Created by Haijun Deng on 13-7-13.
+//  Copyright (c) 2013年 __MyCompanyName__. All rights reserved.
+//
+/*
+ TASK: Running Median
+ Input:
+ 1
+ 2
+ 3
+ 4
+ -1
+ -1
+ 5
+ 6
+ 7
+ -1
+ 0
+ 
+ 2
+ 3
+ -1
+ 0
+ 
+ Output:
+ 2
+ 3
+ 5
+ 
+ 2
+ */
+#include <iostream>
+#include <list>
+
+int main()
+{
+    list<int> l;
+    int n;
+    scanf("%d", &n);
+    while (n!=0) 
+    {
+        if (n>0) 
+        {
+            list.append(n);
+        }
+        else
+        {
+            
+        }
+    }
+    return 0;
+}
