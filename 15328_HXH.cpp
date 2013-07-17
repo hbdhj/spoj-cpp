1ab13c0ef325ce563f6fab1394a7f4619e823384
hbdhj <hbdhj@sina.com>
2013-07-18 00:11:56 +0800

    15328 still WA

--- 15328_HXH.cpp
+++ 15328_HXH.cpp
@@ -49,33 +49,63 @@ int main()
                 }
             }
         }
-        for (i=0; i<n-2; i++) 
-        {
-            for (j=0; j<i+2; j++) 
-            {
-                if (j==0) {
-                    //map[j][i+1-j]+=map[j][i-j];
-                    map[n-1][n-j-2]+=map[n-1][n-j-1];
-                }
-                else if(i+1-j==0)
-                {
-                    //map[j][i+1-j]+=map[j-1][i+1-j];
-                    map[n-2-j][n-1]+=map[n-1-j][n-1];
-                }
-                else
-                {
-                    //map[j][i+1-j]+=map[j-1][i+1-j]>map[j][i-j]?map[j-1][i+1-j]:map[j][i-j];
-                    map[n-1-j][n-1-i+j]+=map[n-2-j][n-1-i+j]>map[j][i-j]?map[j-1][i+1-j]:map[j][i-j];
-                }
-            }
-            for (k=0; k<n; k++) {
-                for (j=0; j<n; j++) {
-                    printf("%7d", map[k][j]);
-                }
-                printf("\n");
+        /*for (i=0; i<n; i++) {
+            for (j=0; j<n; j++) {
+                printf("%7d", map[i][j]);
             }
             printf("\n");
-        }        
+        }
+        printf("\n");*/
+        for (i=1; i<n; i++) {
+            map[0][i]+=map[0][i-1];
+            map[i][0]+=map[i-1][0];
+            map[n-1][n-1-i]+=map[n-1][n-i];
+            map[n-1-i][n-1]+=map[n-i][n-1];
+        }
+        /*for (i=0; i<n; i++) {
+            for (j=0; j<n; j++) {
+                printf("%7d", map[i][j]);
+            }
+            printf("\n");
+        }
+        printf("\n");*/
+        for (i=1; i<n-2; i++) {
+            for (j=1; j<n-1-i; j++) {
+                map[i][j]+=map[i-1][j]>map[i][j-1]?map[i-1][j]:map[i][j-1];
+            }
+        }
+        for (i=n-2; i>1; i--) {
+            for (j=n-2; j>n-i-1; j--) {
+                map[i][j]+=map[i+1][j]>map[i][j+1]?map[i+1][j]:map[i][j+1];
+            }
+        }
+        /*for (i=0; i<n; i++) {
+            for (j=0; j<n; j++) {
+                printf("%7d", map[i][j]);
+            }
+            printf("\n");
+        }
+        printf("\n");*/
+        for (i=1; i<n-1; i++) {
+            map[i][n-i-1]+=map[i-1][n-i-1]>map[i][n-i-2]?map[i-1][n-i-1]:map[i][n-i-2];
+            map[i][n-i-1]+=map[i+1][n-i-1]>map[i][n-i]?map[i+1][n-i-1]:map[i][n-i];
+        }
+        /*for (i=0; i<n; i++) {
+            for (j=0; j<n; j++) {
+                printf("%7d", map[i][j]);
+            }
+            printf("\n");
+        }
+        printf("\n");*/
+        int max = -10000;
+        for (i=0; i<n; i++) {
+            if(map[i][n-1-i]>max)
+                max=map[i][n-1-i];
+        }
+        if (max<0)
+            printf("-1\n");
+        else
+            printf("%d\n",max);
     }
     return 0;
 }
