3852a31939766cb662df8acc5b58a84fcce9ffc9
hbdhj <hbdhj@sina.com>
2013-07-25 21:46:21 +0800

    15437 AC

---
+++ 15437_UCV2013I.cpp
@@ -0,0 +1,21 @@
+//
+//  15437_ UCV2013I.cpp
+//  
+//
+//  Created by Haijun Deng on 13-7-25.
+//  Copyright (c) 2013年 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+#include <math.h>
+
+int main()
+{
+    int r, n;
+    scanf("%d %d", &r, &n);
+    while (r!=0&&n!=0) {
+        printf("%.2f\n", float(r)/sin(2*M_PI/4/n));
+        scanf("%d %d", &r, &n);
+    }
+    return 0;
+}
