e6a0a1cb206dd184e490e5043931b9d06da4bbcc
hbdhj <hbdhj@sina.com>
2013-08-09 23:10:40 +0800

    update 43

--- 43_BOOKS1.cpp
+++ 43_BOOKS1.cpp
@@ -16,40 +16,6 @@ using namespace std;
 
 int p[M+1], a[M+1];
 
-long long search_result(const int& m, const int& n, long long begin_p, long long end_p);
-bool feasible(const int& m, const int& n, const long long& thre);
-void print_result(const int& m, const int& n, const long long& thre);
-
-int main()
-{
-    int num_of_cases;
-    long long sum, avg, max_value, thre;
-    
-    cin >> num_of_cases;
-    
-    for(int counter = 0; counter < num_of_cases; counter++)
-    {
-        int m, n;
-        
-        cin >> m >> n;
-        a[0] = sum = max_value = 0;
-        
-        for(int i = 1; i <= m; i++)
-        {
-            cin >> p[i];
-            max_value = ((max_value > p[i])? max_value : p[i]);
-            sum += p[i];
-            a[i] = a[i-1] + p[i];
-        }
-        
-        avg = sum / n;
-        avg = ((avg > max_value)? avg : max_value);
-        thre = search_result(m, n, avg, sum);
-        print_result(m, n, thre);
-    }
-    
-    return 0;
-}
 long long search_result(const int& m, const int& n, long long begin_p, long long end_p)
 {
     long long temp;
@@ -66,6 +32,7 @@ long long search_result(const int& m, const int& n, long long begin_p, long long
     
     return end_p;
 }
+
 bool feasible(const int& m, const int& n, const long long& thre)
 {
     int k = 0;
@@ -88,6 +55,7 @@ bool feasible(const int& m, const int& n, const long long& thre)
     
     return (k <= n);
 }
+
 void print_result(const int& m, const int& n, const long long & thre)
 {
     int k = 0;
@@ -134,3 +102,37 @@ void print_result(const int& m, const int& n, const long long & thre)
     
     cout << p[m] << endl;
 }
+
+int main()
+{
+    int num_of_cases;
+    long long sum, avg, max_value, thre;
+    
+    cin >> num_of_cases;
+    
+    for(int counter = 0; counter < num_of_cases; counter++)
+    {
+        int m, n;
+        
+        cin >> m >> n;
+        a[0] = sum = max_value = 0;
+        
+        for(int i = 1; i <= m; i++)
+        {
+            cin >> p[i];
+            max_value = ((max_value > p[i])? max_value : p[i]);
+            sum += p[i];
+            a[i] = a[i-1] + p[i];
+        }
+        
+        avg = sum / n;
+        avg = ((avg > max_value)? avg : max_value);
+        thre = search_result(m, n, avg, sum);
+        print_result(m, n, thre);
+    }
+    
+    return 0;
+}
+
+
+
