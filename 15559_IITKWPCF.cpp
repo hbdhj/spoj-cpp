bbe528ea7c695a35c898114fc582709d90da1d05
hbdhj <hbdhj@sina.com>
2013-08-22 21:42:42 +0800

    15559 just begin

---
+++ 15559_IITKWPCF.cpp
@@ -0,0 +1,25 @@
+//
+//  15559_IITKWPCF.cpp
+//  
+//
+//  Created by Haijun Deng on 13-8-22.
+//  Copyright (c) 2013年 __MyCompanyName__. All rights reserved.
+//
+/*
+ TASK: Help Feluda with mathematical equations
+ Input:
+ 3
+ 4
+ 24
+ 100 
+ Output:
+ 1 1
+ 244 1 4 6 124 1 4 6 12
+ 4 1 10 25 50
+ */
+#include <iostream>
+
+int main()
+{
+    
+}
