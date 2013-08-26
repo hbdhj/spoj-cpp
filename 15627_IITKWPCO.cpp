5ea6fa5709283016047ec78aeb35e1c842bfca4b
hbdhj <hbdhj@sina.com>
2013-08-26 22:25:31 +0800

    15627 ongoing

---
+++ 15627_IITKWPCO.cpp
@@ -0,0 +1,30 @@
+//
+//  15627_IITKWPCO.cpp
+//  
+//
+//  Created by Haijun Deng on 13-8-26.
+//  Copyright (c) 2013年 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    while (t--)
+    {
+        int n;
+        scanf("%d", &n);
+        vector<int> nums(n,0);
+        for (int i=0; i<n; i++)
+        {
+            scanf("%d", &nums[n]);
+        }
+    }
+    return 0;
+}
