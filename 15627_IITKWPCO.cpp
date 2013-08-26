a376c9f94ef6931f06139010909ae64619d55f8d
hbdhj <hbdhj@sina.com>
2013-08-26 22:56:45 +0800

    15627 WA

--- 15627_IITKWPCO.cpp
+++ 15627_IITKWPCO.cpp
@@ -10,6 +10,8 @@
 #include <vector>
 #include <algorithm>
 
+#define SIZE 1000001
+
 using namespace std;
 
 int main()
@@ -21,10 +23,53 @@ int main()
         int n;
         scanf("%d", &n);
         vector<int> nums(n,0);
+        vector<int> number(SIZE, 0);
         for (int i=0; i<n; i++)
         {
-            scanf("%d", &nums[n]);
+            scanf("%d", &nums[i]);
+            number[nums[i]]++;
         }
+        int group=0;
+        for (int i=0; i<n; i++)
+        {
+            if(nums[i]%2==0)
+            {
+                if (number[nums[i]/2]&&number[nums[i]])
+                {
+                    if(number[nums[i]/2]>number[nums[i]])
+                    {
+                        group+=number[nums[i]];
+                        number[nums[i]/2]-=number[nums[i]];
+                        number[nums[i]]=0;
+                    }
+                    else
+                    {
+                        group+=number[nums[i]];
+                        number[nums[i]]-=number[nums[i]/2];
+                        number[nums[i]/2]=0;
+                    }
+                }
+            }
+            if(2*nums[i]<SIZE)
+            {
+                if (number[2*nums[i]]&&number[nums[i]])
+                {
+                    if(number[2*nums[i]]>number[nums[i]])
+                    {
+                        group+=number[nums[i]];
+                        number[2*nums[i]]-=number[nums[i]];
+                        number[nums[i]]=0;
+                    }
+                    else
+                    {
+                        group+=number[2*nums[i]];
+                        number[nums[i]]-=number[2*nums[i]];
+                        number[2*nums[i]]=0;
+                    }
+                }
+            }
+        }
+        printf("%d\n", group);
     }
     return 0;
 }
