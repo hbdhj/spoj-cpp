a9b64409b1c94001df9a7b33058a16f272b5d87d
hbdhj <hbdhj@sina.com>
2013-09-07 23:50:40 +0800

    11581 AC

---
+++ 11581_CLOCK2.cpp
@@ -0,0 +1,23 @@
+/*
+TASK: The Famous Clock
+ALGO: ad-hoc
+*/
+
+#include <cstdio>
+#include <cstring>
+
+char a[][8] = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII"};
+
+int main()
+{
+	int i, t=1;
+	char aa[8];
+	while(scanf("%s", aa) == 1) 
+    {
+	    for(i = 0; i < 12; i++) 
+            if(!strcmp(a[i], aa)) 
+                break;
+	    printf("Case %d: %d\n", t++, i+1);
+	}
+	return 0;
+}
