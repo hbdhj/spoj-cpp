577f62e1bdc817d69c1ab424c62d40ffa5eb6af2
hbdhj <hbdhj@sina.com>
2013-09-22 22:50:44 +0800

    just begin

---
+++ 16066_HPERLAT.cpp
@@ -0,0 +1,34 @@
+//
+//  16066_HPERLAT.cpp
+//  
+//
+//  Created by Haijun Deng on 13-9-22.
+//  Copyright (c) 2013年 __MyCompanyName__. All rights reserved.
+//
+/*
+ TASK: Lattice Points Between Hyperbolas
+ Input:
+ 2
+ 2 4
+ 3 10  
+ 
+ Output:
+ Case 1: 14
+ Case 1: 48
+ */
+#include <iostream>
+int main()
+{
+    int t, i, j, m, n, r;
+    scanf("%d", &t);
+    for (i=0; i<t; i++) 
+    {
+        scanf("%d %d", &m, &n);
+        r=0;
+        for (j=m; j<=n; j++)
+        {
+            
+        }
+    }
+    return 0;
+}
