6ce4b77c70c8ef56c38ccd9f00749d6728cc776b
hbdhj <hbdhj@sina.com>
2013-09-26 22:23:50 +0800

    16035 begin

---
+++ 16035_OU7.cpp
@@ -0,0 +1,20 @@
+//
+//  16035_OU7.cpp
+//  
+//
+//  Created by Haijun Deng on 13-9-26.
+//  Copyright (c) 2013年 __MyCompanyName__. All rights reserved.
+//
+/*
+ Input:
+ 5
+ 0 0 5 7.5 1 -3 10.75 -20.1 12.01 -22
+ 6
+ 0 1 19 0 38 1 38 21 19 22 0 21
+ 0
+ 
+ Output:
+ The towers in case 1 can be covered in 3 frequencies.
+ The towers in case 2 can be covered in 2 frequencies.
+ */
+#include <iostream>
