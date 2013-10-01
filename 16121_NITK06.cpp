e60f467df2d8e87a69353ef9ff73c5df56b83782
hbdhj <hbdhj@sina.com>
2013-10-01 22:11:52 +0800

    NITK06 AC

--- 16121_NITK06.cpp
+++ 16121_NITK06.cpp
@@ -14,3 +14,34 @@ NO
 YES
 
  */
+#include <stdio.h>
+
+int main()
+{
+    int t, n, i, m;
+    scanf("%d", &t);
+    while (t--)
+    {
+        scanf("%d", &n);
+        bool ret=true;
+        int left = 0;
+        for (i=0; i<n; i++) 
+        {
+            scanf("%d", &m);
+            if (i==0) 
+                left=m;
+            else
+            {
+                if (m<left) 
+                    ret=false;
+                else
+                    left=(m-left);
+            }
+        }
+        if (ret&&left==0) 
+            printf("YES\n");
+        else
+            printf("NO\n");
+    }
+        return 0;
+}
