335836ddbd5dce49693767e3ce7e16b4b012d40e
hbdhj <hbdhj@sina.com>
2014-10-11 21:25:59 +0800

    begin 904

---
+++ 904_ IMAGE.cpp
@@ -0,0 +1,41 @@
+904. Image Perimeters
+Input:
+2 2 2 2
+XX
+XX
+6 4 2 3
+.XXX
+.XXX
+.XXX
+...X
+..X.
+X...
+5 6 1 3
+.XXXX.
+X....X
+..XX.X
+.X...X
+..XXX.
+7 7 2 6
+XXXXXXX
+XX...XX
+X..X..X
+X..X...
+X..X..X
+X.....X
+XXXXXXX
+7 7 4 4
+XXXXXXX
+XX...XX
+X..X..X
+X..X...
+X..X..X
+X.....X
+XXXXXXX
+0 0 0 0
+Output:
+8
+18
+40
+48
+8
