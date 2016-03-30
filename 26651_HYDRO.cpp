3c14b7603ce88cd3a6833e569163e9c5ba7572a2
Deng Haijun <hbdhj@sina.com>
2016-03-30 22:55:02 +0800

    26651 pass

--- 26651_HYDRO.cpp
+++ 26651_HYDRO.cpp
@@ -1,6 +1,6 @@
 /*
 TASK: HYDRO - Hydroelectric dams
-ALGO: 
+ALGO:
 Input:
 2
 3
@@ -8,6 +8,8 @@ Input:
 6
 3 0 0 2 0 4
 Output:
+2
+10
 */
 #include <stdio.h>
 #include <vector>
@@ -17,26 +19,48 @@ using namespace std;
 int main()
 {
     int t, i, n, j;
-	scanf("%d", &t);
-	for(i=0;i<t;i++)
+    scanf("%d", &t);
+    for(i=0;i<t;i++)
     {
         scanf("%d",&n);
-		vector<int> h(n);
-		for(j=0;j<n;j++)
-		{
-			scanf("%d",&h[j]);
-		}
-		int acme = 0;
-		int water = 0;
-		for(j=0;j<n;j++)
-		{
-			if(h[j]>acme)
-				acme = h[j];
-			else
-				water += acme - h[j];
-			//printf("h[%d] = %d, acme = %d, water = %d\n", j, h[j], acme, water);
-		}
-		printf("%d\n", water);
+        vector<int> h(n);
+        for(j=0;j<n;j++)
+        {
+            scanf("%d",&h[j]);
+        }
+        int water = 0;
+        int p_l = 0;
+        int p_r = n - 1;
+        int max_l = h[p_l];
+        int max_r = h[p_r];
+        while(p_r>p_l)
+        {
+            if (max_l<max_r)
+            {
+                p_l++;
+                if (h[p_l]>=max_l)
+                {
+                    max_l = h[p_l];
+                }
+                else
+                {
+                    water = water + (max_l - h[p_l]);
+                }
+            }
+            else
+            {
+                p_r--;
+                if (h[p_r]>=max_r)
+                {
+                    max_r = h[p_r];
+                }
+                else
+                {
+                    water = water + (max_r - h[p_r]);
+                }
+            }
+        }
+        printf("%d\n", water);
     }
-	return 0;
+    return 0;
 }
